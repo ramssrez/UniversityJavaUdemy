23120256e886135ee5d825cffca2e85175324394
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-07 17:56:35 -0500

    Usi de la clase Scanner

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,22 +1,20 @@
 
+//Importar la clase Scanner para usarla
+import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var nombre = "Karla";
-        //caracter especial de salto de línea       \n
-        System.out.println("Nueva Linea: \n" + nombre);
-
-        //Caracter de tabulador     \t
-        System.out.println("Tabulado: \t" + nombre);
+        System.out.println("Escribe tu nombre: ");
+        //Creación un objeto de la clase Scanner
+        Scanner consola = new Scanner(System.in);
+        // Con esto le estamos asignando a la variables usuario lo que
+        //venga de la consola
         
-        //Caracter de Retroceso     \b
-        System.out.println("Retroceso: \b" + nombre);
-        
-        //Comilla Simple        \'
-        System.out.println("Comilla Simple \'" + nombre + "\'");
-        
-        //Comilla doble         \"      
-        System.out.println("Comilla Doble: \"" + nombre + "\"" );
-
+        //El metodo nextLine() nos permite leer datos de la consola
+        var usuario = consola.nextLine();
+        System.out.println("usuario = " + usuario);
+        System.out.println("Escribe el titulo: ");
+        var titulo = consola.nextLine();
+        System.out.println("Resultado: " + titulo + " " + usuario);
     }
 }
