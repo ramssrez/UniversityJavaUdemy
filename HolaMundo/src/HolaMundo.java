ef4a9c5d1c77109e99bedb0234285b02a2635a1b
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-07 17:32:23 -0500

    concatenacion en Java

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,42 +1,27 @@
 //Mi clase en Java
 public class HolaMundo {
     public static void main(String args[]){
-        //Definición de varables tipo entero
-        int variableEntera = 10;
-        System.out.println(variableEntera); 
-        //Modificación del valor de la variable
-        variableEntera = 5;
-        System.out.println(variableEntera); 
-        //Declaración de variables tipo String
-        String variableCadena = "Saludos desde java";
-        System.out.println(variableCadena);
-        //Ejercicio de cambiar la variableCadena
-        variableCadena = "Adios !!!!!";
-        System.out.println(variableCadena);
-        
-        //Verion 10 de Java se puede utilizar var
-        //var es inferencia de tipos de Java, de esta manera reconoce
-        //el tipo de variable
-        var variableDos = 15;
-        System.out.println(variableDos);
-        
-        //Ejercicio con tipo de dato String
-        var variableCadenaDos ="Nueva Cadena";
-        System.out.println("variableCadenaDos = " + variableCadenaDos);
-        
-        //Reglas para declar variables.
+        var usuario = "Juan";
+        var titulo = "Ingeniero";
+        var union = titulo + " " +usuario;
+        //Conquetanación en Java
+        System.out.println("union = " + union);
+        var i = 3;
+        var j = 4;
+        System.out.println(i+j); //Se realiza la suma de numeros
+        System.out.println(i+j+usuario);
         /*
-        No se puede iniciar con numeros
-        No se puede empezar con mayuscula, por convencion se uso minuscula
-        Es posible usar variables con acento, pero no es recomendaod
-        No se puede utilizar caracteres especiales
-        El guion bajo es posible
-        Usar el simbolo de dolar tambien es posible
+        De izquierda a derecha se evalua las expresiones
+        */
+        System.out.println(usuario +i+j); //Contexto Cadena
+        /*
+        Si primero encuentra una cadena, todo lo demas lo va a concatenar
+        */
+        System.out.println(usuario + (i+j));
+        /*
+        El parentesis cambia la dirección de lo que se esta evaluando
+        dando prioridad al parentesis
         */
         
-        //Las siguientes variables son permitidos
-        var miVariable = 1;
-        var _miVariable = 2;
-        var $miVariable = 3;
     }
 }
