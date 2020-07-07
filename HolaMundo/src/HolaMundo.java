69477f9c196d7124363baeea8382fd4d9308a99c
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-07 16:57:48 -0500

    Uso de var para inferencia de datos

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -7,15 +7,22 @@ public class HolaMundo {
         //Modificación del valor de la variable
         variableEntera = 5;
         System.out.println(variableEntera); 
+        //Declaración de variables tipo String
         String variableCadena = "Saludos desde java";
         System.out.println(variableCadena);
         //Ejercicio de cambiar la variableCadena
         variableCadena = "Adios !!!!!";
         System.out.println(variableCadena);
+        
+        //Verion 10 de Java se puede utilizar var
+        //var es inferencia de tipos de Java, de esta manera reconoce
+        //el tipo de variable
+        var variableDos = 15;
+        System.out.println(variableDos);
+        
+        //Ejercicio con tipo de dato String
+        var variableCadenaDos ="Nueva Cadena";
+        System.out.println("variableCadenaDos = " + variableCadenaDos);
+                
     }
-    /*
-                  Tipos de datos en Java
-       Tipos primitivos        Tipos Referenciados(Object)
-    enteros     flotantes    clases  interfaces arrays
-    */
 }
