ed2cce3ff1cecfb8e7db28a3ceed92f0130f323f
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-07 16:46:55 -0500

    Tipos enteros y string

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -3,7 +3,15 @@ public class HolaMundo {
     public static void main(String args[]){
         //Definición de varables tipo entero
         int variableEntera = 10;
-        System.out.println(variableEntera);        
+        System.out.println(variableEntera); 
+        //Modificación del valor de la variable
+        variableEntera = 5;
+        System.out.println(variableEntera); 
+        String variableCadena = "Saludos desde java";
+        System.out.println(variableCadena);
+        //Ejercicio de cambiar la variableCadena
+        variableCadena = "Adios !!!!!";
+        System.out.println(variableCadena);
     }
     /*
                   Tipos de datos en Java
