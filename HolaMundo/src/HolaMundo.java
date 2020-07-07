632edbe9d5d8a7dbe6a15236bffea3745568d82d
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-07 17:44:34 -0500

    Manejo de caracteres especiales

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,27 +1,22 @@
-//Mi clase en Java
+
 public class HolaMundo {
-    public static void main(String args[]){
-        var usuario = "Juan";
-        var titulo = "Ingeniero";
-        var union = titulo + " " +usuario;
-        //Conquetanación en Java
-        System.out.println("union = " + union);
-        var i = 3;
-        var j = 4;
-        System.out.println(i+j); //Se realiza la suma de numeros
-        System.out.println(i+j+usuario);
-        /*
-        De izquierda a derecha se evalua las expresiones
-        */
-        System.out.println(usuario +i+j); //Contexto Cadena
-        /*
-        Si primero encuentra una cadena, todo lo demas lo va a concatenar
-        */
-        System.out.println(usuario + (i+j));
-        /*
-        El parentesis cambia la dirección de lo que se esta evaluando
-        dando prioridad al parentesis
-        */
+
+    public static void main(String args[]) {
+        var nombre = "Karla";
+        //caracter especial de salto de línea       \n
+        System.out.println("Nueva Linea: \n" + nombre);
+
+        //Caracter de tabulador     \t
+        System.out.println("Tabulado: \t" + nombre);
         
+        //Caracter de Retroceso     \b
+        System.out.println("Retroceso: \b" + nombre);
+        
+        //Comilla Simple        \'
+        System.out.println("Comilla Simple \'" + nombre + "\'");
+        
+        //Comilla doble
+        System.out.println("Comilla Doble: \"" + nombre + "\"" );
+
     }
 }
