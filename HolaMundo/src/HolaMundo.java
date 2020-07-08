021a31408a327ebfea637af3aa482410976a1528
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-08 18:59:01 -0500

    inferencia de datos con var

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,30 +1,15 @@
 
 //Importar la clase Scanner para usarla
-import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        /*
-        Resumiendo, de esta manera se puede obtener los 
-        limites de los numero primitivos
-        */
-        //Valores minimos y máximos del byte
-        System.out.println("Valor minimo byte: " + Byte.MIN_VALUE);
-        System.out.println("Valor máxmo byte: " + Byte.MAX_VALUE);
-        //Valors minimos y maximos del short
-        System.out.println("Valor minimo short: " + Short.MIN_VALUE);
-        System.out.println("Valor minimo short: " + Short.MAX_VALUE);
-        //Valores minimos y maximos de los enteros
-        System.out.println("Valor minimo enteros: " + Integer.MIN_VALUE);
-        System.out.println("Valor minimo entero: " + Integer.MAX_VALUE);
-        //Valores minimos y maximos de los long
-        System.out.println("Valor minimo long: " + Long.MIN_VALUE);
-        System.out.println("Valor minimo long: " + Long.MAX_VALUE);
-        //Tipos primitivos de tipo flotante
-        System.out.println("Valor minimo flotante: " + Float.MIN_VALUE);
-        System.out.println("Valor minimo flotante: " + Float.MAX_VALUE);
-        //Valores minimos y maximos de los double
-        System.out.println("Valor minimo double: " + Double.MIN_VALUE);
-        System.out.println("Valor minimo double: " + Double.MAX_VALUE);
+        var numeroEntero=10;
+        System.out.println("numeroEntero = " + numeroEntero);
+        
+        var numeroDouble = 10.0;
+        System.out.println("numeroDouble = " + numeroDouble);
+        
+        var numeroFloat = 10.0F;
+        System.out.println("numeroFloat = " + numeroFloat);
     }
 }
