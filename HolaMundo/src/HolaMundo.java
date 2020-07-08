f0f895698a5698e882d58b122dd1b60a0b05e473
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-08 17:22:06 -0500

    Uso  de tipos primitivos

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,17 +4,37 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        System.out.println("Escribe tu nombre: ");
-        //Creación un objeto de la clase Scanner
-        Scanner consola = new Scanner(System.in);
-        // Con esto le estamos asignando a la variables usuario lo que
-        //venga de la consola
+        /*
+         Tipos primitivos enteros:
+        Si se va ahorrar espacio es recomendable el bite
+        byte 8 bits   Rango[-128,127]
+        short 16 bits Rango[-32768,32767]
+        int 32 bits   Rango[-2^31,2^31-1]
+        long 64 bits  Rango[-2^63,2^63-1]
+        char 16 bits  Rango[0,65535]
         
-        //El metodo nextLine() nos permite leer datos de la consola
-        var usuario = consola.nextLine();
-        System.out.println("usuario = " + usuario);
-        System.out.println("Escribe el titulo: ");
-        var titulo = consola.nextLine();
-        System.out.println("Resultado: " + titulo + " " + usuario);
+        
+        Tipos prmitivos flotantes
+        float 32 bits Rango[1.4e-0.45, 3.4e+038]
+        double 64 bits Rango[4.9e-324,1.8e+308]
+        boolean       true or false
+        */  
+        byte numeroByte = 10;
+        System.out.println("numeroByte = " + numeroByte);
+        //Parseo de para que se pueda realizar en byte
+        numeroByte =(byte) 129;
+        System.out.println("numeroByte = " + numeroByte);
+        
+        //Valores minimos y máximos del byte
+        System.out.println("Valor minimo byte: " + Byte.MIN_VALUE);
+        System.out.println("Valor máxmo byte: " + Byte.MAX_VALUE);
+        
+        //Valors minimos y maximos del short
+        short numeroShort = 10;
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("Valor minimo short" + Short.MIN_VALUE);
+        System.out.println("Valor minimo short" + Short.MAX_VALUE);
+
+        
     }
 }
