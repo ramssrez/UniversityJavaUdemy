95e2208200f29921808268b535631896ae8295a7
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-09 21:23:34 -0500

    Operadores condicionales && ||

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -5,50 +5,39 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        //Operaciones de asignacion y relacionales
-        var a = 3;
-        var b = 2;
-        //Operador de igualdad ==
-        var c = (a == b); //Es igual igula
-        System.out.println("c = " + c);
-
-        //Operador de desigualdad o diferente de != 
-        var d = a != b;
-        System.out.println("d = " + d);
-
-        //La igualdad no funcina con objetos
-        var cadena1 = "Hola";
-        var cadena2 = "Adios";
-        var e = cadena1 == cadena2; //Compara la referencia de onbetos
-
-        //No se esta comparando las variables, ya que son de tipo string
-        System.out.println("e = " + e);
-        var f = cadena1.equals(cadena2); //Comparamos contenidos de cadenas
-        //Es la manera correcta de igualar unos objetos
-        System.out.println("f = " + f);
-
-        //Operadores relacionales
-        var g = a > b; //Operdor de mayor que
-        System.out.println("g = " + g);
-        g = a >= b;
-        System.out.println("g = " + g); //Operador de mayor o igual que
+        //Operadores condicionales
+        var a =10;
+        var min = 0;
+        var max=10;
+        //uso del ooperador and &&
+        var resultado = a>=min && a<=max;
+        //Resultado es verdadero true y true
+        //si un uno de los operando es falso el resultado es falso
+        System.out.println("resultado = " + resultado);
         
-        //Operación de igualdad
-        if (a % 2 == 0) {
-            System.out.println("Es Número par");
-        } else {
-            System.out.println("Es Número impar");
-        }
-        
-        var edad = 10;
-        var adulto =18;
-        
-        //Operacion relacional
-        if(edad >=adulto){
-            System.out.println("Es un adulto");
+        if(resultado){
+            System.out.println("Dentro de rango");
         }else{
-            System.out.println("Es menor de edad");
+            System.out.println("Fuera de rango");
         }
-             
+        
+        //Otra forma de expresar el if
+         if((a>=min) && (a<=max)){
+            System.out.println("Dentro de rango");
+        }else{
+            System.out.println("Fuera de rango");
+        }
+         
+         //Para el caso del OR, ||, si uno es verdades, el resultado es verdader
+        //Padre puede ir al juego de su hijo
+        var vacaciones = false;
+        var descanso = true;
+        
+        if(vacaciones||descanso){
+            System.out.println("Padre puede ir al juego");
+        }else{
+            System.out.println("Padre no puede ir al juego");
+        }
+       
     }
 }
