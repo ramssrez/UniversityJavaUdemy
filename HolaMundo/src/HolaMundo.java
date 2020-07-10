6f5a6655d6ac190e6100031ba8125abebefe335b
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-09 20:23:33 -0500

    operadores unarios para incrementos post y pre y negacion boolean

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,30 +4,36 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-       //Operadores de asignación
-       int a = 3, b=2;
-       int c = a +5 -b;
-       //La asignación va de izquierda a derecha, se le agnación de c
-        System.out.println("c = " + c);
-       
-       a+=1;//a = a+1 operadorde composición de suma
-        System.out.println("composición suma a = " + a);
-        a +=3; //a=a+3
+        
+        //Operadores Unarios
+        int a = 3;
+        var b = -a;
         System.out.println("a = " + a);
+        System.out.println("b = " + b);
         
-        a-=2; //Operador composicion de resta
-        //a=a-2
-        System.out.println("composición resta a = " + a);
-        a/=2; //Operador de composición de división
-        System.out.println("composición división a = " + a);
+        //operador de negación, tipo oolean
+        var c= true;
+        var d = !c; //Operador de negación !, solo para boleanos
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
         
-        a*=3; //Operador de composición de multiplicación
-        System.out.println("composición multiplicación a = " + a);
-        
-        a%=2; //Operador de composición de modulo
-        System.out.println("composición modulo a = " + a);
-     
- 
+        //Operdor de incrementos
+          //Preincremento (simbolo antes de la variable)
+          var e =3;
+          var f =++e; //Primero se incrementa la variable y despues se usa 
+          System.out.println("e = " + e);
+          System.out.println("f = " + f);
+          
+          //Posincremento ( despues de la variable)
+          var g=5;
+          var h=g++; //Primero se utiliza el valor de la varible y despues se incrementa
+          System.out.println("g = " + g);
+          System.out.println("h = " + h);
+          
+          
+          
+          
         
     }
+    
 }
