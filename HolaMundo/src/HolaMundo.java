e596c5a3c98ef0e5aa6850f2ea05ddbe48ea5cf0
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-09 19:49:29 -0500

    operadores mas, menos, división, multiplicación, modulo

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,37 +4,29 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-       //Convertir un tipo String a tipo int con el metodo parseInt
-       var edad = Integer.parseInt("20");
-        System.out.println("edad = " + edad);
-        System.out.println("edad = " + (edad+1));
+       int a=3,b=2;
+       var resultado = a + b;  //Operador suma
+       System.out.println("resultado = " + resultado);
+       
+       resultado = a - b;     //Operador resta
+       System.out.println("resultado de la resta = " + resultado);
         
-        var edadDos ="20";
-        System.out.println("edadDos = " + edadDos);
-        //No realiza la suma, solo concatena con el numero 1
-        System.out.println("edadDos = " + (edadDos+1));
+       resultado = a * b;    //Operador multiplicación
+       System.out.println("resultado de la multiplicación= " +resultado );
         
-        var valorPi = Double.parseDouble("3.1416");
-        System.out.println("valorPi = " + valorPi);
+       resultado = a/b;//Resultado en enteros
+       System.out.println("resultado de la división= " + resultado);
         
-        //Pedir un valor
-//        System.out.println("Ingrese tu edad: ");
-        var consola = new Scanner(System.in);
-//        edad = Integer.parseInt(consola.nextLine());
-//        System.out.println("Edad = " + edad );
-//        
-        //Conversion del tipo string al tipo int
-        var edadTexto = String.valueOf(10);
-        System.out.println("edadTexto = " + edadTexto);
+       var resultadoDos =3.0 / b;    //operador división
+       System.out.println("resultado de la división = " + resultadoDos);
         
-        //Con el metodo charAt(index), nos regresa el valor
-        //que se le ha asignado a ese indice
-        var caracter = "Hola".charAt(0);
-        System.out.println("caracter = " + caracter);
-        
-        System.out.println("Proporciona un caracter");
-        //Estamos parseando de tipo caracter en donde solo tomara uno de ellos
-        caracter = consola.nextLine().charAt(0);
-        System.out.println("caracter = " + caracter);
+       resultado = a%b; //Operador modulo
+       System.out.println("resultado del modulo= " + resultado);
+       
+       if (a%2==0){
+           System.out.println("Es número par");
+       }else{
+           System.out.println("Es número impar");
+       }
     }
 }
