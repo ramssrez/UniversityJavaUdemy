55ac3fffd061005ec770489e09d90974d820f631
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-13 18:21:03 -0500

    Sentencia switch con periodos

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -5,19 +5,55 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        var mes = 11;
-        if (mes == 1 || mes == 2 || mes == 12){
-            System.out.println("Estas en Invierno");
-            
-        }else if(mes == 3|| mes == 4|| mes == 5 ){
-            System.out.println("Estas en Primavera");
-        }else if (mes == 6|| mes == 7 || mes ==8){
-            System.out.println("Estas en Verano");
-        }else if (mes == 9|| mes == 10|| mes == 11){
-            System.out.println("Estas en Otoño");
-        }else {
-            System.out.println("Este es no es un mes del año");
+        //Sentencia switch es mas para los menus
+        var numero =5;
+        var numeroTexto = "Valor desconocido";
+        
+        switch(numero){
+            case 1:
+                numeroTexto = "Numero Uno";
+                break;
+            case 2:
+                numeroTexto="Numero Dos";
+                break;
+            case 3:
+                numeroTexto="Numero tres";
+                break;
+            case 4:
+                numeroTexto="Numero Cuatro";
+                break;
+            default:
+                numeroTexto="Caso no encontrado";
         }
+        System.out.println("numeroTexto = " + numeroTexto);
+        
+        var mes =1;
+        var estacion = "Estacion no valida";
+        switch(mes){
+            case 1:
+            case 2:
+            case 12:
+                estacion = "Invierno";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                estacion ="Primavera";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                estacion = "Verano";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                estacion="Otoño";
+                break;
+            default:
+            System.out.println("Estacion no valida"); 
+        }
+        System.out.println("estacion = " + estacion);
         
     }
 }
