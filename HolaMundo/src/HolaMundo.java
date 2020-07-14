cf7af1144b6a59cf65076e4718cdbf826cd3b2de
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-13 21:11:54 -0500

    Clases en Java

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -5,48 +5,6 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        //Uso de ciclos(while)
-        var contador = 0;
-        //Se puede hacer uso de una variable para poder observar la variable
-        while(contador <3){
-            System.out.println("contador = " + contador);
-            contador++;
-        }
-        
-        //Uso de ciclo do/while
-        var conta = 0;
-        do{
-            System.out.println("conta = " + conta);
-            conta++;
-        }while(conta<3);
-        
-        //Uso del ciclo for con break
-        for(var contadorDos = 0;contadorDos<3;contadorDos++){
-            if(contadorDos%2 == 0){
-                System.out.println("contadorDos = " + contadorDos);
-                break;//Hace que el ciclo se termine
-            }
-            
-        }
-
-        //Cambiando la logica de programacion usando continue
-        for (var contadorTres =0;contadorTres<3;contadorTres++){
-            if(contadorTres%2 != 0){
-                continue;
-            }
-            System.out.println("contadorTres = " + contadorTres);    
-        }
-        
-        //Programación de tipo go to, en donde se realiza
-        //un punto en donde debe de continuar las lineas de codigo
-        inicio://Etiquetas para que continue el uso de etiquetas
-        for (var contadorCuatro=0;contadorCuatro<=3;contadorCuatro++){
-            if(contadorCuatro %2 != 0){
-               continue inicio;  //Fin de etiquetas
-            }
-            System.out.println("contadorCuatro = " + contadorCuatro);
-            
-        }
-        
+                        
     }
 }
---
+++ HolaMundo/src/clases/Persona.java
@@ -0,0 +1,13 @@
+package clases;
+/*
+Inicio de la parte de la creacion de clases
+en este comenzamos relizando en un paquete llamado clases
+si no existe el paquete nos va a marcar un error
+*/
+
+//palabra reservada public para poder tener acceso a esta clase
+public class Persona {
+    
+
+    
+}
