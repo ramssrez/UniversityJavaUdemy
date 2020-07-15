9f260b870fc6c95313319cd4a4db7069744eb1c3
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-15 17:49:39 -0500

    Cambio de nombre del proyecto y creacion de la clase aritmetica

---
+++ HolaMundo/src/operaciones/Aritmetica.java
@@ -0,0 +1,18 @@
+package operaciones;
+
+public class Aritmetica {
+    int a;
+    int b;
+    
+    
+    //Declaracion del metodo
+    public void sumar (){
+        int suma = a+b;
+        System.out.println("suma:  " + suma );
+    }
+    
+    public void restar(){
+        int resta = a-b;
+        System.out.println("resta = " + resta);
+    }
+}
