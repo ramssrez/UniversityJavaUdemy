268a838e0dca23ccfbc7117b7b4d10513cfeebff
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-15 19:58:10 -0500

    Memoria heap y stack

--- HolaMundo/src/operaciones/PruebaAritmetica.java
+++ HolaMundo/src/operaciones/PruebaAritmetica.java
@@ -1,4 +1,9 @@
 package operaciones;
+/*
+Todas las variables locales se agregan en la memoria stack
+Lo objetos se crean en la memoria heap
+los atributos del objeto tambien se crean en la memoria heap
+*/
 public class PruebaAritmetica {
     public static void main(String [] args){
         //Variables locales y su alcance, solo hasta el metodos
@@ -23,7 +28,11 @@ public class PruebaAritmetica {
         Aritmetica aritmeticaDos = new Aritmetica(10, 15);
         System.out.println("aritmeticaDos a = " + aritmeticaDos.a);
         System.out.println("aritmeticaDos b = " + aritmeticaDos.b);
-
+        
+       // aritmetica= null; //Damos fin del ciclo de vida del objeto
+       //Metodo para ejecutor el recolector de asura
+        //System.gc();
+       
     } 
     public static void miMetodo(){
         //La variales esta fuera del alcance donde fue definida
