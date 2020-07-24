6fdc29a5877e5c7e92985f7695586ce5b4eff50b
ramssrez <ramssrez@gmail.com>
2020-07-24 17:26:31 -0500

    Segunda parte de la palabra this

--- LaboratorioCaja/src/palabrathis/PalabraThis.java
+++ LaboratorioCaja/src/palabrathis/PalabraThis.java
@@ -17,8 +17,24 @@ class Persona {
     String apellido;
 
     Persona(String nombre, String apellido) {
+        //super(); llamada al constructor de la clase padre (Objest)
+        System.out.println("Constructor de la clase Persona");
         this.apellido = apellido;
         this.nombre=nombre;
+        System.out.println("Objeto persona con la palabra this:" + this);
+        new Imprimir().imprimir(this); //Imprimir imprimir = new Imprimir();
+        
     }
     
+}
+
+class Imprimir{
+    public Imprimir(){
+        super();// constructor de la clase object (padre) para reservar memoria
+    }
+    public void imprimir(Persona persona){
+        System.out.println("persona desde imprimir" + persona);
+        //El this de esta parte es para el objeto de tipo imprimir
+        System.out.println("Impresión del objeto actual (this)= " + this);
+    }
 }
