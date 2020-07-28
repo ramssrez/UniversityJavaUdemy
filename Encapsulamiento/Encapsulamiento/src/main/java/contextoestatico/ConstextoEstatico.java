aafa2aa3e9fdcca20ac52822962fadad927e902e
ramssrez <ramssrez@gmail.com>
2020-07-27 20:06:28 -0500

    Contexto estatico y dinámico

---
+++ Encapsulamiento/Encapsulamiento/src/main/java/contextoestatico/ConstextoEstatico.java
@@ -0,0 +1,16 @@
+/*
+El contexto estatico es cuando estamos trabajando con las clases
+(Carga de clases)
+El contexto estatico no puede entrar al contexto dinámico
+
+
+
+Ya cuando esta cargada la clase a la memoria, se le conoce como contexto dinámico
+(Carga de objetos)
+*/
+
+package contextoestatico;
+
+public class ConstextoEstatico {
+    
+}
