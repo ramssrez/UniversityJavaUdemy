75d67f870482fe08843ed3c16a964fc865b91e0b
ramssrez <ramssrez@gmail.com>
2020-10-04 15:42:45 -0500

    Primer programa en Java

---
+++ PrimerPrgramaJava/src/main/java/TiposDatos/PrimerJava.java
@@ -0,0 +1,19 @@
+package TiposDatos;
+
+public class PrimerJava {
+    public static void main(String[] args) {
+        //Declaraciónn de variables de tipo primitivo
+        
+        int tipoInt = 10;
+        boolean tipoBoolean = true;
+        float  tipoFloat= 14.15f;
+        double tipoDouble = 1458585622D;
+        
+
+        System.out.println("tipoEntero = " + tipoInt);        
+        System.out.println("tipoBooleano = " + tipoBoolean);        
+        System.out.println("tipoFlotante = " + tipoFloat);   
+        System.out.println("tipoDouble = " + tipoDouble);        
+        
+    }
+}
