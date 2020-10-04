cee610448470b80c1808180d39ef87ace0cf7fd5
ramssrez <ramssrez@gmail.com>
2020-10-04 15:54:11 -0500

    declaración de tipos de variable

--- PrimerProgramaJava/src/main/java/TiposDatos/PrimerJava.java
+++ PrimerProgramaJava/src/main/java/TiposDatos/PrimerJava.java
@@ -3,13 +3,18 @@ package TiposDatos;
 public class PrimerJava {
     public static void main(String[] args) {
         //Declaraciónn de variables de tipo primitivo
-        
         int tipoInt = 10;
         boolean tipoBoolean = true;
         float  tipoFloat= 14.15f;
         double tipoDouble = 1458585622D;
+        short tipoShort = 11000;
+        long tipoLong = 99999999999L;
+        byte tipoByte = 125;
         
-
+        //Impresión de pantalla de las variables previamente declaradas
+        System.out.println("tipoByte = " + tipoByte);
+        System.out.println("tipoLong = " + tipoLong);
+        System.out.println("tipoShort = " + tipoShort);
         System.out.println("tipoEntero = " + tipoInt);        
         System.out.println("tipoBooleano = " + tipoBoolean);        
         System.out.println("tipoFlotante = " + tipoFloat);   
