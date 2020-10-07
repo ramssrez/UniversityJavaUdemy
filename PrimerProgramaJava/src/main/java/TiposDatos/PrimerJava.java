4f224c995ef31e29dea4245a9d0c43ec8e01b956
ramssrez <ramssrez@gmail.com>
2020-10-07 12:55:32 -0500

    delcaración de valores constantes

--- PrimerProgramaJava/src/main/java/TiposDatos/PrimerJava.java
+++ PrimerProgramaJava/src/main/java/TiposDatos/PrimerJava.java
@@ -1,10 +1,13 @@
 package TiposDatos;
 
 public class PrimerJava {
-    public static void main(String[] args) {
+    //Declaración de la variable PI
+    static final double PI = 3.1416; 
+    public static void main(String[] args) {  
+        final double PO = 3.1416;
        if(true){
            int variable = 10;
-           int a = 11;
+           int a = 11;  
            System.out.println("a = " + a);
            System.out.println("variable = " + variable);
        }
