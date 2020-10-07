0bd65ecfe474498da6bcbc3d5ada62f0ab03709c
ramssrez <ramssrez@gmail.com>
2020-10-07 12:45:42 -0500

    delcalaración de variables en java

--- PrimerProgramaJava/src/main/java/TiposDatos/PrimerJava.java
+++ PrimerProgramaJava/src/main/java/TiposDatos/PrimerJava.java
@@ -2,23 +2,17 @@ package TiposDatos;
 
 public class PrimerJava {
     public static void main(String[] args) {
-        //Declaraciónn de variables de tipo primitivo
-        int tipoInt = 10;
-        boolean tipoBoolean = true;
-        float  tipoFloat= 14.15f;
-        double tipoDouble = 1458585622D;
-        short tipoShort = 11000;
-        long tipoLong = 99999999999L;
-        byte tipoByte = 125;
-        
-        //Impresión de pantalla de las variables previamente declaradas
-        System.out.println("tipoByte = " + tipoByte);
-        System.out.println("tipoLong = " + tipoLong);
-        System.out.println("tipoShort = " + tipoShort);
-        System.out.println("tipoEntero = " + tipoInt);        
-        System.out.println("tipoBooleano = " + tipoBoolean);        
-        System.out.println("tipoFlotante = " + tipoFloat);   
-        System.out.println("tipoDouble = " + tipoDouble);        
-        
+       if(true){
+           int variable = 10;
+           int a = 11;
+           System.out.println("a = " + a);
+           System.out.println("variable = " + variable);
+       }
+       /*
+       //Necesitamos acceder a esta varible, pero no 
+       //ha sido declarada para su creación ya que esta
+       //delimitada por las llaves del if
+        System.out.println(a);
+       */
     }
 }
