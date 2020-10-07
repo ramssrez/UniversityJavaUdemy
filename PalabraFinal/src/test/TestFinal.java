464dc2b66e94a07128c4aae533b97e4695b7ff92
ramssrez <ramssrez@gmail.com>
2020-10-07 17:50:40 -0500

    creación del ejercicico final

---
+++ PalabraFinal/src/test/TestFinal.java
@@ -0,0 +1,9 @@
+
+package test;
+
+public class TestFinal {
+    public static void main(String[] args) {
+        final int variable = 10;
+        System.out.println("variable = " + variable);
+    }
+}
