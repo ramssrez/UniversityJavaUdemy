074bc280058aa58594ca57440a81cc39b62a44cb
ramssrez <ramssrez@gmail.com>
2020-10-07 20:38:30 -0500

    Uso de otras clases para obtener la fecha de nacimiento

--- DPO1_U2_A2_RARP/src/Clases/Estudiante.java
+++ DPO1_U2_A2_RARP/src/Clases/Estudiante.java
@@ -1,26 +1,40 @@
 package Clases;
 
+import java.text.DateFormat;
 import java.util.Date;
 
 public class Estudiante {
-    private String name;
+    private String nombre;
     private String apellido;
     private Date fechaNacimiento;
     private int matricula;
     private float promedio;
     private long telefono;
     private String direccion;
+    private int edad;
+    private Date fechaActual;
+    private DateFormat fechaNacimientoDos;
 
     public Estudiante (){
         System.out.println("Esto es el constructor de la clase");
     }
-
-    public String getName() {
-        return name;
+    
+    public int getEdad(){
+        return edad;
+    }
+    
+    public int calcularEdad(Date fecha){
+        System.out.println("Esto es la fecha actual desde la clase estudiante: " + fecha);
+        edad = 25;
+        return edad;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getName() {
+        return nombre;
+    }
+
+    public void setName(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getApellido() {
@@ -71,13 +85,19 @@ public class Estudiante {
         this.telefono = telefono;
     }
     
-    
-    
-    
+    public void setFechaActual (Date fechaActual){
+        this.fechaActual = fechaActual;
+    }
 
     @Override
     public String toString() {
-        return "Estudiante{" + "name=" + name + ", apellido=" + apellido + ", fechaNacimiento=" + fechaNacimiento + ", matricula=" + matricula + ", promedio=" + promedio + ", telefono=" + telefono + ", direccion=" + direccion + '}';
+        return "Estudiante{" + "name=" + nombre + ", apellido=" + apellido + ", fechaNacimiento=" + fechaNacimiento + ", matricula=" + matricula + ", promedio=" + promedio + ", telefono=" + telefono + ", direccion=" + direccion + ", edad=" + edad + '}';
+    }
+
+    public void setFechaNacimiento(DateFormat date) {
+        
+        this.fechaNacimientoDos = date;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
--- DPO1_U2_A2_RARP/src/main/Principal.java
+++ DPO1_U2_A2_RARP/src/main/Principal.java
@@ -1,28 +1,41 @@
 package main;
 
 import Clases.Estudiante;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Principal {
     public static void main(String[] args) {
+        Date fecha = new Date();
+        DateFormat date = new SimpleDateFormat("dd/MM/YYYY");
         Estudiante estudiante = new Estudiante();
-        estudiante.toString();
+        
         estudiante.setName("Raúl");
         estudiante.setApellido("Ramírez");
         estudiante.setMatricula(308122598);
         estudiante.setDireccion("Cerrada Baja California 1");
         estudiante.setPromedio(9.3f);
         estudiante.setTelefono(5555555555L);
+        estudiante.calcularEdad(fecha);
         
-        Date fecha = new Date();
         //Calendar fechaDos = new Calendar() ;
+        DateTimeFormatter fmt =DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate fechaNacimiento = LocalDate.parse("27/06/1992",fmt);
+        LocalDate ahora = LocalDate.now();
         
-        System.out.println("El día es: " + fecha.getDay());
-        System.out.println("El tiempo es: " + fecha.getTime());
-        System.out.println("El mes es: " + fecha.getMonth());
+        Period periodo = Period.between(fechaNacimiento, ahora);
+        System.out.println("Tu edad es: " + periodo.getYears() + "años");
+        System.out.println(periodo.getMonths() + " meses");
+        System.out.println(periodo.getDays() + " dias");
+        
+        estudiante.setFechaNacimiento(fecha);
         System.out.println("La fecha es: " + fecha);
         System.out.println("Estudiante: " +estudiante.toString());
-        
+        System.out.println("date = " + date.format(fecha));
     }
 }
