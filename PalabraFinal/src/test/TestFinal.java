27d6336b4240ee3226f66463cd0732409d19b658
ramssrez <ramssrez@gmail.com>
2020-10-07 19:17:05 -0500

    Eliminar el proyecto de final

--- PalabraFinal/src/test/TestFinal.java
+++
@@ -1,9 +0,0 @@
-
-package test;
-
-public class TestFinal {
-    public static void main(String[] args) {
-        final int variable = 10;
-        System.out.println("variable = " + variable);
-    }
-}
