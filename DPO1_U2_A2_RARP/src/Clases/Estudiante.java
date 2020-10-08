033b6be388e91360fe360c4ec63ee9e78e199c26
ramssrez <ramssrez@gmail.com>
2020-10-07 19:29:14 -0500

    Creacion de la clase principal y la clase estudiante

--- DPO1_U2_A2_RARP/src/Clases/Estudiante.java
+++ DPO1_U2_A2_RARP/src/Clases/Estudiante.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Clases;
 
-/**
- *
- * @author ramssrez
- */
+import java.util.Date;
+
 public class Estudiante {
+    private String name;
+    private String apellido;
+    private Date fechaNacimiento;
+    private int matricula;
+    private float promedio;
+    private int telefono;
+    private String direccion;
+
+    public Estudiante (){
+        System.out.println("ESto es el constructor de la clase");
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "name=" + name + ", apellido=" + apellido + ", fechaNacimiento=" + fechaNacimiento + ", matricula=" + matricula + ", promedio=" + promedio + ", telefono=" + telefono + ", direccion=" + direccion + '}';
+    }
     
 }
---
+++ DPO1_U2_A2_RARP/src/main/Principal.java
@@ -0,0 +1,12 @@
+package main;
+
+import Clases.Estudiante;
+
+public class Principal {
+    public static void main(String[] args) {
+        Estudiante estudiante = new Estudiante();
+        estudiante.toString();
+        System.out.println("Estudiante: " +estudiante.toString());
+        
+    }
+}
