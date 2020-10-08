e11d522e328f351836deb89aa52087d3ad947c5f
ramssrez <ramssrez@gmail.com>
2020-10-07 19:53:59 -0500

    Creacion de los metodos getters y setters

--- DPO1_U2_A2_RARP/src/Clases/Estudiante.java
+++ DPO1_U2_A2_RARP/src/Clases/Estudiante.java
@@ -8,12 +8,70 @@ public class Estudiante {
     private Date fechaNacimiento;
     private int matricula;
     private float promedio;
-    private int telefono;
+    private long telefono;
     private String direccion;
 
     public Estudiante (){
-        System.out.println("ESto es el constructor de la clase");
+        System.out.println("Esto es el constructor de la clase");
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public float getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(float promedio) {
+        this.promedio = promedio;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+    
+    
     
     
 
--- DPO1_U2_A2_RARP/src/main/Principal.java
+++ DPO1_U2_A2_RARP/src/main/Principal.java
@@ -1,11 +1,27 @@
 package main;
 
 import Clases.Estudiante;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Principal {
     public static void main(String[] args) {
         Estudiante estudiante = new Estudiante();
         estudiante.toString();
+        estudiante.setName("Raúl");
+        estudiante.setApellido("Ramírez");
+        estudiante.setMatricula(308122598);
+        estudiante.setDireccion("Cerrada Baja California 1");
+        estudiante.setPromedio(9.3f);
+        estudiante.setTelefono(5555555555L);
+        
+        Date fecha = new Date();
+        //Calendar fechaDos = new Calendar() ;
+        
+        System.out.println("El día es: " + fecha.getDay());
+        System.out.println("El tiempo es: " + fecha.getTime());
+        System.out.println("El mes es: " + fecha.getMonth());
+        System.out.println("La fecha es: " + fecha);
         System.out.println("Estudiante: " +estudiante.toString());
         
     }
