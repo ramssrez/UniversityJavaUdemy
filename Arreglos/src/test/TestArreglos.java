68dd99746b85d0715b344f0dfd9d733088f780d2
ramssrez <ramssrez@gmail.com>
2020-10-09 17:06:00 -0500

    agregar valores al arreglo y poderlo imprimirlos

--- Arreglos/src/test/TestArreglos.java
+++ Arreglos/src/test/TestArreglos.java
@@ -7,7 +7,11 @@ public class TestArreglos {
         int edades[] = new int[3];
         //Declaraicion = Intancia dentro del corchete se pone el número de elementos
         System.out.println("edades = " + Arrays.toString(edades));//Método toString para el objeto Array
-               
+        
+        //Asignación de valores a los arreglos
+        edades[0]=10;
+        //Acceder al valor del arreglo
+        System.out.println("edades = " + edades[0]);
     }
     
 }
