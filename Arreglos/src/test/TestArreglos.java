0ce7982937f660d5c228ee65fd26875e131c5cf2
ramssrez <ramssrez@gmail.com>
2020-10-09 16:57:47 -0500

    impresion del objeto array

--- Arreglos/src/test/TestArreglos.java
+++ Arreglos/src/test/TestArreglos.java
@@ -1,5 +1,13 @@
 package test;
 
+import java.util.Arrays;
+
 public class TestArreglos {
+    public static void main(String[] args) {
+        int edades[] = new int[3];
+        //Declaraicion = Intancia dentro del corchete se pone el número de elementos
+        System.out.println("edades = " + Arrays.toString(edades));//Método toString para el objeto Array
+               
+    }
     
 }
