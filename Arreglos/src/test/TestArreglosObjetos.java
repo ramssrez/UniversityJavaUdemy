387222f589ca7847fb553fe994dba37e21788f2b
ramssrez <ramssrez@gmail.com>
2020-10-12 21:37:04 -0500

    Uso de for para llenar e imprimir objetos de tipo persona

--- Arreglos/src/test/TestArreglosObjetos.java
+++ Arreglos/src/test/TestArreglosObjetos.java
@@ -2,6 +2,7 @@
 package test;
 
 import domain.Persona;
+import java.util.Scanner;
 
 public class TestArreglosObjetos {
     public static void main(String[] args) {
@@ -11,5 +12,21 @@ public class TestArreglosObjetos {
         
         System.out.println("persona [0] = " + personas[0]);
         System.out.println("persona [1] = " + personas[1]);
+        
+        for (int i = 0; i<personas.length;i++){
+            Scanner sc = new Scanner(System.in);
+            String nombre;
+            int edad;
+            System.out.print("Ingresa el nombre de la persona " + i +":");
+            nombre = sc.nextLine();
+            System.out.print("Ingresa la edad de la persona " + i +":");
+            edad = sc.nextInt();
+            personas[i]=new Persona(nombre,edad);
+            System.out.println("----------------------------------------------------");
+        }
+        
+        for (int i = 0; i<personas.length;i++){
+            System.out.println("Persona " + i + ": "+ personas[i]);
+        }
     }
 }
