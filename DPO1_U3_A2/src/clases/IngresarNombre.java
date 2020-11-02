84cda9b8c4b5beedeec98b9fb902879b112a8b3d
ramssrez <ramssrez@gmail.com>
2020-11-02 14:21:53 -0600

    Ingresar nombre como clase

---
+++ DPO1_U3_A2/src/clases/IngresarNombre.java
@@ -0,0 +1,22 @@
+package clases;
+
+public class IngresarNombre {
+
+    private String nombre;
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+     public void agregarNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void agregarNombre(String nombre, String apellidoPaterno) {
+        this.nombre = nombre + apellidoPaterno;
+    }
+
+    public void agregarNombre(String nombre, String apellidoPaterno, String apellidoMaterno) {
+        this.nombre = nombre + apellidoPaterno + apellidoMaterno;
+    }
+
+}
