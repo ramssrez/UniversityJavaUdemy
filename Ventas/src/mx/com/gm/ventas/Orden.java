cacee45c7019b81d9588aa77d1b6d1c6b06f9a4d
ramssrez <ramssrez@gmail.com>
2020-11-02 08:32:45 -0600

    Creación de la clase Orden

---
+++ Ventas/src/mx/com/gm/ventas/Orden.java
@@ -0,0 +1,16 @@
+package mx.com.gm.ventas;
+
+public class Orden {
+
+    private final int idOrden;
+    private Producto productos[];
+    private static int contadorOrdenes;
+    private int contadorProductos;
+    private static final int MAX_PRODUCTOS = 10;
+
+    public Orden() {
+        this.idOrden = ++Orden.contadorOrdenes;
+        this.productos = new Producto[Orden.MAX_PRODUCTOS];
+    }
+
+}
