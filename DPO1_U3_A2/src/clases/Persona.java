caff6b7cd762e1cb1c3bda79a9fd355e1b554f68
ramssrez <ramssrez@gmail.com>
2020-11-01 18:53:47 -0600

    Creación e instancia del objeto persona

--- DPO1_U3_A2/src/clases/Persona.java
+++ DPO1_U3_A2/src/clases/Persona.java
@@ -1,5 +1,14 @@
 package clases;
 
 public class Persona {
-    
+   private String nombre;
+   private String apellidoPateeno;
+   private String apellidoMaterno;
+   private int edad;
+
+    public Persona() {
+        System.out.println("Creacion del objeto Persona");
+    }
+   
+   
 }
--- DPO1_U3_A2/src/main/Principal.java
+++ DPO1_U3_A2/src/main/Principal.java
@@ -1,5 +1,9 @@
 package main;
 
+import clases.Persona;
+
 public class Principal {
-    
+    public static void main(String[] args) {        
+        Persona persona = new Persona();
+    }
 }
