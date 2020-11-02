9deb48647982bfa52b7218c96211c2e7dc1779d3
ramssrez <ramssrez@gmail.com>
2020-11-01 20:47:49 -0600

    Creacion del proyecto Ventas

---
+++ Ventas/src/mx/com/gm/ventas/Producto.java
@@ -0,0 +1,11 @@
+package mx.com.gm.ventas;
+
+public class Producto {
+    private int idProducto;
+    private String nombre;
+    private double precio;
+    private int contadorProductos;
+    
+    
+   
+}
