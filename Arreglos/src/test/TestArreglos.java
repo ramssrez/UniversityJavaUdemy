4c3e0fa76c74cc3eb477845501ae0321d4762f1d
ramssrez <ramssrez@gmail.com>
2020-11-01 20:19:57 -0600

    creación de la matriz con los parametros a imprimir

--- Arreglos/src/test/TestArreglos.java
+++ Arreglos/src/test/TestArreglos.java
@@ -3,22 +3,41 @@ package test;
 import java.util.Arrays;
 
 public class TestArreglos {
+
     public static void main(String[] args) {
         int edades[] = new int[3];
         //Declaraicion = Intancia dentro del corchete se pone el número de elementos
         System.out.println("edades = " + Arrays.toString(edades));//Método toString para el objeto Array
-        
+
         //Asignación de valores a los arreglos
-        edades[0]=10;
+        edades[0] = 10;
         //Acceder al valor del arreglo
         System.out.println("edades = " + edades[0]);
-        
+
         //Error en tiempo de ejecución
         //edades[3]=5;
-        for (int i =0 ; i < edades.length;i++){
-            System.out.println("Edades elemento " + i + ": "+ edades[i]);
-            
+        for (int i = 0; i < edades.length; i++) {
+            System.out.println("Edades elemento " + i + ": " + edades[i]);
         }
+
+        //Creación de una matriz para ejempleficarlo en Java}
+        int edadesMatriz[][] = new int[3][2];
+        System.out.println("edadesMatriz = " + edadesMatriz);
+        edadesMatriz[0][0] = 5;
+        edadesMatriz[0][1] = 3;
+        edadesMatriz[1][0] = 7;
+        edadesMatriz[1][1] = 8;
+
+        System.out.println("edadesMatriz 0-0 = " + edadesMatriz[0][0]);
+        System.out.println("edadesMatriz 0-1 = " + edadesMatriz[0][1]);
+
+        for (int renglon = 0; renglon < edadesMatriz.length; renglon++) {
+            for (int columna = 0; columna < edadesMatriz[renglon].length; columna++) {
+                System.out.println("Edades Matriz: " + renglon + "-" + columna + ": " + edadesMatriz[renglon][columna]);
+
+            }
+        }
+
     }
-    
+
 }
