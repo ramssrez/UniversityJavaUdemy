227b767b259a8fa69fcbe2f2c76bc064f5c0e52e
ramssrez <ramssrez@gmail.com>
2020-11-02 08:23:31 -0600

    creacion de la clase Producto

--- Ventas/src/mx/com/gm/ventas/Producto.java
+++ Ventas/src/mx/com/gm/ventas/Producto.java
@@ -1,11 +1,49 @@
 package mx.com.gm.ventas;
 
 public class Producto {
-    private int idProducto;
+
+    private final int idProducto;
     private String nombre;
     private double precio;
-    private int contadorProductos;
+    private static int contadorProductos;
+
+    //Constructor vacio que genera un identificador para cada objeto creado
+    private Producto() {
+        this.idProducto = ++Producto.contadorProductos;
+    }
+
+    //Constructor con argumentos
+    public Producto(String nombre, double precio) {
+        //Llamar el constructor privado con la palabra reservada this
+        this();
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "idProducto=" + idProducto + ", nombre=" + nombre + ", precio=" + precio + '}';
+    }
+
     
-    
-   
 }
