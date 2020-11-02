ff4ded1573b139054f5a3b1ccd6ecb82989a6b00
ramssrez <ramssrez@gmail.com>
2020-11-02 09:39:56 -0600

    creacion de la clase main para pruebas

---
+++ Ventas/src/mx/com/gm/ventas/test/VentasTest.java
@@ -0,0 +1,17 @@
+
+package mx.com.gm.ventas.test;
+
+import mx.com.gm.ventas.*;
+
+public class VentasTest {
+    public static void main(String[] args) {
+        Producto producto1 = new Producto("Camisa",50.00);
+        Producto producto2 = new Producto("Pantaon",100.00);
+        
+        Orden orden1 = new Orden();
+        orden1.agregarProducto(producto1);
+        orden1.agregarProducto(producto2);
+        orden1.mostrarOrden();
+        
+    }
+}
