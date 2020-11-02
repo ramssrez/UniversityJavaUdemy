281d538d8472ee3eaaa00bc0595f5169e745b692
ramssrez <ramssrez@gmail.com>
2020-11-01 19:32:18 -0600

    Creacion de metodos getter y setter y la instancia de objetos tipo personas

--- DPO1_U3_A2/src/clases/Persona.java
+++ DPO1_U3_A2/src/clases/Persona.java
@@ -2,13 +2,68 @@ package clases;
 
 public class Persona {
    private String nombre;
-   private String apellidoPateeno;
+   private String apellidoPaterno;
    private String apellidoMaterno;
    private int edad;
 
     public Persona() {
         System.out.println("Creacion del objeto Persona");
     }
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Persona(String nombre, String apellidoPaterno, String apellidoMaterno) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public Persona(String nombre, String apellidoPateeno, String apellidoMaterno, int edad) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPateeno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "Nombre: " + nombre + ", Apellido Paterno: " + apellidoPaterno + ", Apellido Materno: " + apellidoMaterno + ", Edad: " + edad + '}';
+    }
+    
    
    
 }
--- DPO1_U3_A2/src/main/Principal.java
+++ DPO1_U3_A2/src/main/Principal.java
@@ -3,7 +3,18 @@ package main;
 import clases.Persona;
 
 public class Principal {
-    public static void main(String[] args) {        
+
+    public static void main(String[] args) {
         Persona persona = new Persona();
+        System.out.println("persona = " + persona);
+
+        Persona personaDos = new Persona("Jesus");
+        System.out.println("personaDos = " + personaDos);
+
+        Persona personaTres = new Persona("Jesus", "Esquivel", "Hernandez");
+        System.out.println("personaTres = " + personaTres);
+
+        Persona personaCuatro = new Persona("Jesus","Esquivel","Hernandez",25);
+        System.out.println("personaCuatro = " + personaCuatro);
     }
 }
