241262e2bcdd081bde453a87c939fffe4f02675d
ramssrez <ramssrez@gmail.com>
2020-11-03 12:20:07 -0600

    Creacion del proyecto Excepcion

---
+++ Excepciones/src/main/Principal.java
@@ -0,0 +1,22 @@
+package main;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        double resultado = 0;
+
+        try {
+            resultado = 10 / 0;
+
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error" + e.getMessage());
+            e.printStackTrace(System.out);
+        }
+        System.out.println("resultado = " + resultado);
+    }
+
+    public static double dividir(double dividiendo, double divisor) {
+        return (dividiendo / divisor);
+    }
+
+}
