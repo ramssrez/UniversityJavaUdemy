3276a100fcbc76cf2ce00c21fee2ef80d016f5e2
ramssrez <ramssrez@gmail.com>
2020-11-03 12:54:56 -0600

    Creacion de diferentes try/catch para observar las diferencias de cada una

--- Excepciones/src/main/Principal.java
+++ Excepciones/src/main/Principal.java
@@ -1,19 +1,31 @@
 package main;
 
 import static aritmetica.Aritmetica.division;
+import excepcion.OperadorExcepcion;
 
 public class Principal {
 
     public static void main(String[] args) {
         double resultado = 0;
-
-//        try {
-            resultado = division(10, 0);
-
-//        } catch (Exception e) {
-//            System.out.println("Ocurrio un error: " + e.getMessage());
-//            e.printStackTrace(System.out);
-//        }
+        int numeros[] = {1,4,2,3};
+        
+        try {
+            //resultado = division(10, 0);
+            System.out.println(numeros[10]);
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Ocurrio un error de tipo ArrayIndex " + e.getMessage());
+        }catch(ArithmeticException e){
+            System.out.println("Ocurrio un error de ArithmeticaException " + e.getMessage());
+        }catch(OperadorExcepcion e){
+            System.out.println("Ocurrio un erro de tipo Operacion Exception " + e.getMessage());
+        }catch (Exception e) {
+            System.out.println("Ocurrio un error de tipo Exception: " + e.getMessage());
+            //e.printStackTrace(System.out);
+        }finally{
+            System.out.println("Se realizo la divición entre cero ");
+        }
+        
+        
         System.out.println("resultado = " + resultado);
     }
 
