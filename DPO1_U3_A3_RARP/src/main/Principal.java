409c2bc9017837333660b30066fcbf2e19e561fe
ramssrez <ramssrez@gmail.com>
2020-11-03 12:01:41 -0600

    ejmeplo de try/catch

--- DPO1_U3_A3_RARP/src/main/Principal.java
+++ DPO1_U3_A3_RARP/src/main/Principal.java
@@ -1,14 +1,23 @@
 package main;
 
 public class Principal {
+
     public static void main(String[] args) {
-        double dividendo = 50.0;
-        double divisor = 0;
-        System.out.println("El resultado dividir " + dividendo +" entre " + divisor+ " es: " + dividir(dividendo, divisor));
+        double resultado;
+
+        try {
+            resultado = 10 / 0;
+            System.out.println("resultado = " + resultado);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("El try catch ha terminado");
+        }
+
     }
-    
-    public static double dividir (double dividiendo, double divisor){
-        return(dividiendo/divisor);
+
+    public static double dividir(double dividiendo, double divisor) {
+        return (dividiendo / divisor);
     }
- 
+
 }
