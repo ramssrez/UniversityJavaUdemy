897a0e2fef2b69816e4d684932a9f1bc3fb72527
ramssrez <ramssrez@gmail.com>
2020-11-03 11:17:44 -0600

    Antes del uso de try catch

--- DPO1_U3_A3_RARP/src/main/Principal.java
+++ DPO1_U3_A3_RARP/src/main/Principal.java
@@ -1,5 +1,14 @@
 package main;
 
 public class Principal {
+    public static void main(String[] args) {
+        double dividendo = 50.0;
+        double divisor = 0;
+        System.out.println("El resultado dividir " + dividendo +" entre " + divisor+ " es: " + dividir(dividendo, divisor));
+    }
     
+    public static double dividir (double dividiendo, double divisor){
+        return(dividiendo/divisor);
+    }
+ 
 }
