e2bfaafd6bb29bcb370d1ff2623765291926a5f5
ramssrez <ramssrez@gmail.com>
2020-11-04 14:05:48 -0600

    Implementación para Oracle

---
+++ ClasesAbstractas/src/accesodatos/ImpllementacionOracle.java
@@ -0,0 +1,25 @@
+package accesodatos;
+
+public class ImpllementacionOracle implements IAccesodatos {
+
+    @Override
+    public void insertar() {
+        throw new UnsupportedOperationException("NO esta soportado aún"); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void listar() {
+        System.out.println("Insertar desde Oracle");
+    }
+
+    @Override
+    public void actualizar() {
+        System.out.println("Insertar desde Oracle");
+    }
+
+    @Override
+    public void eliminar() {
+        System.out.println("Insertar desde Oracle");
+    }
+
+}
