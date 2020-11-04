5958dfaaee95038d56ca5e4fb593307679499239
ramssrez <ramssrez@gmail.com>
2020-11-04 12:34:22 -0600

    Creación de las clases abtractas

--- ClasesAbstractas/src/domain/FiguraGeometrica.java
+++ ClasesAbstractas/src/domain/FiguraGeometrica.java
@@ -1,5 +1,27 @@
 package domain;
 
-public class FiguraGeometrica {
-    
+public abstract class FiguraGeometrica {
+
+    private String tipoFigura;
+
+    protected FiguraGeometrica(String tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
+    //No se abren argumentos, solo se declara el método sin procedimiento
+    public abstract void dibujar();
+
+    public String getTipoFigura() {
+        return tipoFigura;
+    }
+
+    public void setTipoFigura(String tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
+    @Override
+    public String toString() {
+        return "FiguraGeometrica{" + "tipoFigura=" + tipoFigura + '}';
+    }
+
 }
