49e3e33a6db3df4bef3c8d9f8286be64dd087c6b
ramssrez <ramssrez@gmail.com>
2020-11-04 14:00:53 -0600

    Creacion de la interfase y su implementacion

--- ClasesAbstractas/src/accesodatos/IAccesodatos.java
+++ ClasesAbstractas/src/accesodatos/IAccesodatos.java
@@ -1,5 +1,14 @@
 package accesodatos;
 
 public interface IAccesodatos {
-    
+
+    int MAX_REGISTROS = 10;
+
+    void insertar();
+
+    void listar();
+
+    void actualizar();
+
+    void eliminar();
 }
--- ClasesAbstractas/src/accesodatos/ImplementacionMySql.java
+++ ClasesAbstractas/src/accesodatos/ImplementacionMySql.java
@@ -1,6 +1,28 @@
-
 package accesodatos;
 
-public class ImplementacionMySql implements IAccesodatos{
-    
+public class ImplementacionMySql implements IAccesodatos {
+
+    @Override
+    public void insertar() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Insertar desde MySql");
+    }
+
+    @Override
+    public void listar() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Listar desde MySql");
+    }
+
+    @Override
+    public void actualizar() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Actualizar desde MySql");
+    }
+
+    @Override
+    public void eliminar() {
+        throw new UnsupportedOperationException("No se encuentra soportado aun."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
