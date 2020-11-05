e83ded298f32f229805cee0174f31345b8940207
ramssrez <ramssrez@gmail.com>
2020-11-04 21:15:58 -0600

    Creacion de la clase computadora

--- MundoPc/src/mx/com/gm/mundopc/Computadora.java
+++ MundoPc/src/mx/com/gm/mundopc/Computadora.java
@@ -1,5 +1,9 @@
 package mx.com.gm.mundopc;
 
 public class Computadora {
-    
+    private int idComputadora;
+    private String nombre;
+    private Monitor monitor;
+    private Raton raton;
+    private Teclado teclado;
 }
