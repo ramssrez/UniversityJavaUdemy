cf68b12cbf91b158d7642a0c2043a9299c048a36
ramssrez <ramssrez@gmail.com>
2020-11-04 21:08:44 -0600

    Creacíón de la clase Monitor

---
+++ MundoPc/src/mx/com/gm/mundopc/Monitor.java
@@ -0,0 +1,9 @@
+package mx.com.gm.mundopc;
+
+public class Monitor {
+    private final int idMonitor;
+    private String marca;
+    private double size;
+    private static int contadorMonitor;
+    
+}
