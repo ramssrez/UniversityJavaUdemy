487beff0ddc04c87daa80e9abecf47224111b719
ramssrez <ramssrez@gmail.com>
2020-11-04 21:20:08 -0600

    Creacion del constructor de la clase Computadora

--- MundoPc/src/mx/com/gm/mundopc/Computadora.java
+++ MundoPc/src/mx/com/gm/mundopc/Computadora.java
@@ -1,9 +1,22 @@
 package mx.com.gm.mundopc;
 
 public class Computadora {
+
     private int idComputadora;
     private String nombre;
     private Monitor monitor;
     private Raton raton;
     private Teclado teclado;
+    private static int contadorComputadoras;
+
+    private Computadora() {
+        this.idComputadora = ++ Computadora.contadorComputadoras;
+    }
+    
+    public Computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton){
+        this();
+        
+    }
+    
+
 }
