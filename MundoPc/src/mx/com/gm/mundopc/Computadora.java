307cddcda8d9b821710ca30cfdeeba7699cff398
ramssrez <ramssrez@gmail.com>
2020-11-04 21:24:04 -0600

    Creacion de la clase Computadora

--- MundoPc/src/mx/com/gm/mundopc/Computadora.java
+++ MundoPc/src/mx/com/gm/mundopc/Computadora.java
@@ -2,7 +2,7 @@ package mx.com.gm.mundopc;
 
 public class Computadora {
 
-    private int idComputadora;
+    private final int idComputadora;
     private String nombre;
     private Monitor monitor;
     private Raton raton;
@@ -15,8 +15,53 @@ public class Computadora {
     
     public Computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton){
         this();
-        
+        this.nombre = nombre;
+        this.monitor = monitor;
+        this.teclado = teclado;
+        this.raton = raton;
+    }
+
+    public int getIdComputadora() {
+        return idComputadora;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public Raton getRaton() {
+        return raton;
+    }
+
+    public void setRaton(Raton raton) {
+        this.raton = raton;
+    }
+
+    public Teclado getTeclado() {
+        return teclado;
+    }
+
+    public void setTeclado(Teclado teclado) {
+        this.teclado = teclado;
     }
     
+
+    @Override
+    public String toString() {
+        return "Computadora{" + "idComputadora=" + idComputadora + ", nombre=" + nombre + ", monitor=" + monitor + ", raton=" + raton + ", teclado=" + teclado + '}';
+    }
 
 }
