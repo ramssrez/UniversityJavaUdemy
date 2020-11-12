773b77c502aa1918ff4d4637ec6fe4a4e660e216
ramssrez <ramssrez@gmail.com>
2020-11-12 12:55:44 -0600

    Creación del menú del programa

--- DPO1_U4_A2_RARP/src/main/Principal.java
+++ DPO1_U4_A2_RARP/src/main/Principal.java
@@ -1,10 +1,45 @@
 package main;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
 
     public static void main(String[] args) {
+        boolean salir = false;
+        int opcion; //Guardaremos la opcion del usuario
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Ingresa alguna de las opciones para ingresar los salarios de los jefes de cada área");
+        while (!salir) {
+            System.out.println("1.- Regitrar salarios de los módulos");
+            System.out.println("2.- Imprimir salarios de los módulos");
+            System.out.println("3.- Salir");
+            try {
+                System.out.print("Selecciona una de la opciones: ");
+                opcion = scan.nextInt();
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Opción 1");
+                        break;
+                    case 2:
+                        System.out.println("Opción 2");
+                        break;
+                    case 3:
+                        System.out.println("Salir");
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Ingresa un valor entre 1 y 3");
+                        break;
+                }
+            }catch(InputMismatchException e){
+                System.out.println("Debes de ingresar un números");
+                scan.next();
+            }
+
+        }
+
         String areas[] = {"Administración", "Operaciones", "Recursos Humanos"};
         int modulos[] = {1, 2};
         double salario[][] = new double[modulos.length][areas.length];
@@ -24,7 +59,7 @@ public class Principal {
             System.out.println("Modulo " + (i + 1) + ": ");
             for (int j = 0; j < areas.length; j++) {
                 System.out.println("Departamento de " + areas[j]);
-                System.out.println("Jefe encargado gana: $1500" + salario[i][j]);
+                System.out.println("Jefe encargado gana: $" + salario[i][j]);
             }
             System.out.println("****************************************************************************************+*******");
         }
