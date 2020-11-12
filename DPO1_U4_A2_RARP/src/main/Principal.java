c57b4272e66708a04a96a6694ba72af3004523a7
ramssrez <ramssrez@gmail.com>
2020-11-12 13:14:24 -0600

    Creacion de los métodos para retorno

--- DPO1_U4_A2_RARP/src/main/Principal.java
+++ DPO1_U4_A2_RARP/src/main/Principal.java
@@ -9,6 +9,9 @@ public class Principal {
         boolean salir = false;
         int opcion; //Guardaremos la opcion del usuario
         Scanner scan = new Scanner(System.in);
+        String areas[] = {"Administración", "Operaciones", "Recursos Humanos"};
+        int modulos[] = {1, 2};
+        double salario[][] = new double[modulos.length][areas.length];
 
         System.out.println("Ingresa alguna de las opciones para ingresar los salarios de los jefes de cada área");
         while (!salir) {
@@ -21,6 +24,7 @@ public class Principal {
                 switch (opcion) {
                     case 1:
                         System.out.println("Opción 1");
+                        salario = ingresarSalarios(areas, modulos);
                         break;
                     case 2:
                         System.out.println("Opción 2");
@@ -33,18 +37,39 @@ public class Principal {
                         System.out.println("Ingresa un valor entre 1 y 3");
                         break;
                 }
-            }catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("Debes de ingresar un números");
                 scan.next();
             }
 
         }
 
-        String areas[] = {"Administración", "Operaciones", "Recursos Humanos"};
-        int modulos[] = {1, 2};
+//        double salario[][] = new double[modulos.length][areas.length];
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Ingresa los valores que se te solicitan ");
+//        for (int i = 0; i < modulos.length; i++) {
+//            System.out.println("Modulo " + (i + 1) + ": ");
+//            for (int j = 0; j < areas.length; j++) {
+//                System.out.println("Departamento de " + areas[j]);
+//                System.out.print("Ingresa el salario del jefe: $");
+//                salario[i][j] = scanner.nextDouble();
+//            }
+//            System.out.println("****************************************************************************************+*******");
+//        }
+        for (int i = 0; i < modulos.length; i++) {
+            System.out.println("Modulo " + (i + 1) + ": ");
+            for (int j = 0; j < areas.length; j++) {
+                System.out.println("Departamento de " + areas[j]);
+                System.out.println("Jefe encargado gana: $" + salario[i][j]);
+            }
+            System.out.println("****************************************************************************************+*******");
+        }
+
+    }
+
+    public static double[][] ingresarSalarios(String areas[], int modulos[]) {
         double salario[][] = new double[modulos.length][areas.length];
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingresa los valores que se te solicitan ");
         for (int i = 0; i < modulos.length; i++) {
             System.out.println("Modulo " + (i + 1) + ": ");
             for (int j = 0; j < areas.length; j++) {
@@ -54,15 +79,10 @@ public class Principal {
             }
             System.out.println("****************************************************************************************+*******");
         }
+        return salario;
+    }
 
-        for (int i = 0; i < modulos.length; i++) {
-            System.out.println("Modulo " + (i + 1) + ": ");
-            for (int j = 0; j < areas.length; j++) {
-                System.out.println("Departamento de " + areas[j]);
-                System.out.println("Jefe encargado gana: $" + salario[i][j]);
-            }
-            System.out.println("****************************************************************************************+*******");
-        }
+    public static void retornarSalarios() {
 
     }
 }
