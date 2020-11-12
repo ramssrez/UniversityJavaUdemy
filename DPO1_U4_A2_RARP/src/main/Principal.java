dea2558278b74c4a9038ae686fdd24fa51f1b77a
ramssrez <ramssrez@gmail.com>
2020-11-12 14:41:12 -0600

    Creación del método para imprimir el arreglo bidimencional

--- DPO1_U4_A2_RARP/src/main/Principal.java
+++ DPO1_U4_A2_RARP/src/main/Principal.java
@@ -11,7 +11,8 @@ public class Principal {
         Scanner scan = new Scanner(System.in);
         String areas[] = {"Administración", "Operaciones", "Recursos Humanos"};
         int modulos[] = {1, 2};
-        double salario[][] = new double[modulos.length][areas.length];
+        double salario[][] = null;
+        //new double[modulos.length][areas.length];
 
         System.out.println("Ingresa alguna de las opciones para ingresar los salarios de los jefes de cada área");
         while (!salir) {
@@ -23,51 +24,47 @@ public class Principal {
                 opcion = scan.nextInt();
                 switch (opcion) {
                     case 1:
-                        System.out.println("Opción 1");
+                        System.out.println("-----------------------------------------------------------------------------------------------------------------------");
                         salario = ingresarSalarios(areas, modulos);
+                        System.out.println("-----------------------------------------------------------------------------------------------------------------------");
                         break;
                     case 2:
-                        System.out.println("Opción 2");
+                        System.out.println("-----------------------------------------------------------------------------------------------------------------------");
+                        if (salario == null) {
+                            System.out.println("No se han ingresado salarios, vuelve a la opción 1");
+                        } else {
+                            retornarSalarios(areas, modulos, salario);
+                        }
+                        System.out.println("-----------------------------------------------------------------------------------------------------------------------");
                         break;
                     case 3:
-                        System.out.println("Salir");
+                        System.out.println("-----------------------------------------------------------------------------------------------------------------------");
+                        System.out.println("Has seleccionado la opción 3");
+                        System.out.println("Gracias por tu visita. Adiós!!!!!");
                         salir = true;
+                        System.out.println("-----------------------------------------------------------------------------------------------------------------------");
                         break;
                     default:
-                        System.out.println("Ingresa un valor entre 1 y 3");
+                        System.out.println("-----------------------------------------------------------------------------------------------------------------------");
+                        System.out.println("ERROR, el número no corresponde al rango de 1 a 7");
+                        System.out.println("-----------------------------------------------------------------------------------------------------------------------");
                         break;
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Debes de ingresar un números");
+                System.out.println("-----------------------------------------------------------------------------------------------------------------------");
+                System.out.println("Debes de ingresar un número");
+                System.out.println("-----------------------------------------------------------------------------------------------------------------------");
                 scan.next();
             }
 
         }
 
-//        double salario[][] = new double[modulos.length][areas.length];
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Ingresa los valores que se te solicitan ");
-//        for (int i = 0; i < modulos.length; i++) {
-//            System.out.println("Modulo " + (i + 1) + ": ");
-//            for (int j = 0; j < areas.length; j++) {
-//                System.out.println("Departamento de " + areas[j]);
-//                System.out.print("Ingresa el salario del jefe: $");
-//                salario[i][j] = scanner.nextDouble();
-//            }
-//            System.out.println("****************************************************************************************+*******");
-//        }
-        for (int i = 0; i < modulos.length; i++) {
-            System.out.println("Modulo " + (i + 1) + ": ");
-            for (int j = 0; j < areas.length; j++) {
-                System.out.println("Departamento de " + areas[j]);
-                System.out.println("Jefe encargado gana: $" + salario[i][j]);
-            }
-            System.out.println("****************************************************************************************+*******");
-        }
-
     }
 
     public static double[][] ingresarSalarios(String areas[], int modulos[]) {
+        System.out.println("Has seleccionado la opcion 1");
+        System.out.println("Ingresa los salarios de los jefes de cada área");
+        System.out.println("****************************************************************************************+*******");
         double salario[][] = new double[modulos.length][areas.length];
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < modulos.length; i++) {
@@ -82,7 +79,18 @@ public class Principal {
         return salario;
     }
 
-    public static void retornarSalarios() {
+    public static void retornarSalarios(String areas[], int modulos[], double salario[][]) {
+        System.out.println("Has seleccionado la opcion 2");
+        System.out.println("La información de los salarios se presenta a continuación");
+        System.out.println("****************************************************************************************+*******");
+        for (int i = 0; i < modulos.length; i++) {
+            System.out.println("Modulo " + (i + 1) + ": ");
+            for (int j = 0; j < areas.length; j++) {
+                System.out.println("Departamento de " + areas[j]);
+                System.out.println("Jefe encargado gana: $" + salario[i][j]);
+            }
+            System.out.println("****************************************************************************************+*******");
+        }
 
     }
 }
