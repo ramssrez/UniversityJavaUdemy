31bfd7eb0eac85b16bd7a1fd7fbde88b3eb957f5
ramssrez <ramssrez@gmail.com>
2020-11-30 16:57:33 -0600

    Creacion del método de argumentos variables

--- ArgumentosVariables/src/main/java/test/TestArgumentosVariables.java
+++ ArgumentosVariables/src/main/java/test/TestArgumentosVariables.java
@@ -1,5 +1,16 @@
 package test;
 
 public class TestArgumentosVariables {
+
+    public static void main(String[] args) {
+        imprimirNumeros(3,4,5);
+        imprimirNumeros(1,2,5);
+    }
     
+    //Método que recibe argumentos variables, no es necesario saber el número exacto
+    private static void imprimirNumeros(int... numeros){
+        for (int i = 0; i <numeros.length; i++) {
+            System.out.println("Elemento: " + numeros[i]);
+        }
+    }
 }
