e5b48e4ada45570cd57771c0b8f52240b6a7f78b
ramssrez <ramssrez@gmail.com>
2020-11-30 17:11:01 -0600

    observación de los varargs

--- ArgumentosVariables/src/main/java/test/TestArgumentosVariables.java
+++ ArgumentosVariables/src/main/java/test/TestArgumentosVariables.java
@@ -5,6 +5,7 @@ public class TestArgumentosVariables {
     public static void main(String[] args) {
         imprimirNumeros(3,4,5);
         imprimirNumeros(1,2,5);
+        variosParametros("Juan", 2,3,4,5);
     }
     
     //Método que recibe argumentos variables, no es necesario saber el número exacto
@@ -13,4 +14,20 @@ public class TestArgumentosVariables {
             System.out.println("Elemento: " + numeros[i]);
         }
     }
+    
+    //El parámetro de entrada de los argumentos de varibales tiene que estar al final de los variables 
+    private static void variosParametros(String nombre, int... numeros){
+        System.out.println("Nombre: " + nombre);
+        imprimirNumeros(numeros);
+    }
+    
+    //Este es el mismo ejemplo pero con el vararg al inicio marcando un error
+    /*
+       private static void variosParametrosDos( int... numeros, String nombre){
+        System.out.println("Nombre: " + nombre);
+        imprimirNumeros(numeros);
+    }7
+    */
+
+    
 }
