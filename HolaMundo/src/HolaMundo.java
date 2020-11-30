59c02cfee7b06ca9f91cced0ba6f83382f536929
ramssrez <ramssrez@gmail.com>
2020-11-30 11:51:51 -0600

    limpieza de la rama principal

--- HolaMundo/src/HolaMundo.java
+++
@@ -1,9 +0,0 @@
-//Mi clase en Java
-public class HolaMundo {
-    //Definiendo la función main para que se puda ejecutar
-    //los diferentes argumentos
-    public static void main(String args[]){
-        //Con esta sentenia mandamos texto a java
-        System.out.println("Hola mundo desde Java");
-    }
-}
