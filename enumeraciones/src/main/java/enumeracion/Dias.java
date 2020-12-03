6ec766d5efa945a56b8c40e639b740edb57fd38b
ramssrez <ramssrez@gmail.com>
2020-12-03 13:13:50 -0600

    Creación de la clase test

--- enumeraciones/src/main/java/enumeracion/Dias.java
+++ enumeraciones/src/main/java/enumeracion/Dias.java
@@ -2,5 +2,11 @@
 package enumeracion;
 //Diferencia con la clase class con la enum, es como inicia su sintaxis
 public enum Dias {    
-    
+    LUNES, 
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO
 }
