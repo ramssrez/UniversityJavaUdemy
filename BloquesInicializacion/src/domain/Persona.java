d817cb1eeae4c5e04be6f5c22527d6bd2239cf65
ramssrez <ramssrez@gmail.com>
2020-12-13 21:05:59 -0600

    Creación de la clase persona para observar la inicialización de bloques

--- BloquesInicializacion/src/domain/Persona.java
+++ BloquesInicializacion/src/domain/Persona.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domain;
 
-/**
- *
- * @author ramssrez
- */
 public class Persona {
+    //Bloque de inicializacion estaticos y no estaticos antes del constructor
+
+    private final int idPersona;
+    private static int contadorPersona;
+
+    //Este es una blosque de inicialización estatico
+    static {
+        System.out.println("Ejecución bloque statico");
+        ++Persona.contadorPersona;
+        //No es una variable estatica, por lo que no se puede realizar el proceso
+        //idPersona = 10;
+    }
+
+    //este es un bloque de inicialización no estático, o dinamico, debe ser antes del constructor
+    {
+        System.out.println("Ejecución bloque no estatico");
+        this.idPersona = Persona.contadorPersona++;
+    }
+    
+    public Persona(){
+        System.out.println("Ejecución del constructor"); 
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
     
 }
