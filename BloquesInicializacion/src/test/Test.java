bbac80e71edaada604339007cd190dd769a0faf5
ramssrez <ramssrez@gmail.com>
2020-12-13 21:48:21 -0600

    Uso de Unboxing y Autoboxing para las clases envolventes

--- BloquesInicializacion/src/test/Test.java
+++ BloquesInicializacion/src/test/Test.java
@@ -10,12 +10,11 @@ public class Test {
         System.out.println("");
         Persona persona2 = new Persona();
         System.out.println("persona2 = " + persona2);
-        
-        
+
         //El siguiente bloque es para hacer uso de ForEach para Java
         //Para nuestro caso, es el uso de los arreglos y como recorrerlos
-        int edades[] = {5,8,9,4,3,1,6,4};
-        Persona personas[] = {persona1,persona2, new Persona(), new Persona()};
+        int edades[] = {5, 8, 9, 4, 3, 1, 6, 4};
+        Persona personas[] = {persona1, persona2, new Persona(), new Persona()};
         //Uso anterior para recorrer un arreglo
         for (int i = 0; i < edades.length; i++) {
             System.out.println("i = " + edades[i]);
@@ -25,9 +24,34 @@ public class Test {
         for (int edad : edades) {
             System.out.println("edad = " + edad);
         }
-        
-        for (Persona persona:personas){
+
+        for (Persona persona : personas) {
             System.out.println("persona = " + persona);
         }
+
+        //El siguiente codigo es para el uso de Unboxing y Autoboxing
+        //Clases envolventes de tipo primitivos
+        /*
+        int - Integer
+        long - Long
+        float- Float
+        double - Double
+        boolen - Boolean
+        byte - Byte
+        char - Character
+        short - Short
+    //Las ventajas que presenta es que contienen métodos que podemos utilizar
+         */
+        int entero = 10;
+        System.out.println("entero = " + entero);
+        //Esta conversión se le conoce como Autoboxing
+        Integer enterodos = 10;
+        System.out.println("enterodos = " + enterodos.doubleValue());
+        
+        //Por otra parte la opción de Unboxing es lo inverso a lo anterior, 
+        //Estrayendo el valor anterior
+        entero = enterodos;
+        System.out.println("entero = " + entero);
+        
     }
 }
