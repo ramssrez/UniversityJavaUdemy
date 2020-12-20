85812264c72175ab396c82362bb99cecc4e5021e
ramssrez <ramssrez@gmail.com>
2020-12-20 13:33:25 -0600

    Creacíón de la clase 2

--- BloquesInicializacion/src/paquete1/Clase1.java
+++ BloquesInicializacion/src/paquete1/Clase1.java
@@ -3,14 +3,23 @@ package paquete1;
 
 
 public class Clase1 {
-    public String atributoPublico ="valor atriuto publico"  ;
+    //Protected no esta en el nivel de la clase
+    public String atributoPublico ="valor atriuto publico";
+    protected String atributoProtected = "Valos atributo protected";
     
-    public Clase1 (){
+    protected Clase1 (){
+        System.out.println("Constructor protected");
+    }
+    public Clase1 (String args){
         System.out.println("Constructor publico");
     }
     
-    public void metodoPublico(){
+    public void metodoPublico(String args){
         System.out.println("Esto es un metodo publico");
+    }
+    
+    protected void metodoProtected(){
+        System.out.println("Esto es un método protected");
     }
     
 }
--- BloquesInicializacion/src/test/TestModificadores.java
+++ BloquesInicializacion/src/test/TestModificadores.java
@@ -5,10 +5,10 @@ import paquete1.Clase1;
 public class TestModificadores {
     public static void main(String[] args) {
         System.out.println("");
-        Clase1 clase1 = new Clase1();
+        Clase1 clase1 = new Clase1("Publico");
         //Tenemos acceso al atributo publico de la clase que se ha declarado en otro paquete
         System.out.println("");
         System.out.println("clase1 = " + clase1.atributoPublico);
-        clase1.metodoPublico();
+        clase1.metodoPublico("Publico");
     }
 }
