3aefaf73a20a41ae959c545e80a14792edddf687
ramssrez <ramssrez@gmail.com>
2020-12-20 13:17:21 -0600

    Pruebas del atributo de la clase

--- BloquesInicializacion/src/paquete1/Clase1.java
+++ BloquesInicializacion/src/paquete1/Clase1.java
@@ -3,5 +3,6 @@ package paquete1;
 
 
 public class Clase1 {
+    public String atributoPublico ="valor atriuto publico"  ;
     
 }
--- BloquesInicializacion/src/test/TestModificadores.java
+++ BloquesInicializacion/src/test/TestModificadores.java
@@ -1,6 +1,13 @@
 package test;
+
+import paquete1.Clase1;
+
 public class TestModificadores {
     public static void main(String[] args) {
-        System.out.println("hola HARLINGY");
+        Clase1 clase1 = new Clase1();
+        //Tenemos acceso al atributo publico de la clase que se ha declarado en otro paquete
+        
+        System.out.println("clase1 = " + clase1.atributoPublico);
+        
     }
 }
