0b3e7994a8d1958feef44ac092d78d9d91149569
ramssrez <ramssrez@gmail.com>
2020-12-20 13:20:21 -0600

    uso de constructor publico de una clase

--- BloquesInicializacion/src/paquete1/Clase1.java
+++ BloquesInicializacion/src/paquete1/Clase1.java
@@ -5,4 +5,7 @@ package paquete1;
 public class Clase1 {
     public String atributoPublico ="valor atriuto publico"  ;
     
+    public Clase1 (){
+        System.out.println("Constructor publico");
+    }
 }
--- BloquesInicializacion/src/test/TestModificadores.java
+++ BloquesInicializacion/src/test/TestModificadores.java
@@ -4,9 +4,10 @@ import paquete1.Clase1;
 
 public class TestModificadores {
     public static void main(String[] args) {
+        System.out.println("");
         Clase1 clase1 = new Clase1();
         //Tenemos acceso al atributo publico de la clase que se ha declarado en otro paquete
-        
+        System.out.println("");
         System.out.println("clase1 = " + clase1.atributoPublico);
         
     }
