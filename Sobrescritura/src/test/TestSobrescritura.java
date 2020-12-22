60a6dae6d6885aceee29f22175eab206e89ca085
ramssrez <ramssrez@gmail.com>
2020-12-22 17:28:30 -0600

    Uso de polimorfismo en caso de clases padres he hijos

--- Sobrescritura/src/test/TestSobrescritura.java
+++ Sobrescritura/src/test/TestSobrescritura.java
@@ -1,13 +1,27 @@
 
 package test;
 
-import clases.Gerente;
+import clases.*;
 
 
 public class TestSobrescritura {
     public static void main(String[] args) {
-        Gerente gerente = new Gerente("Jose" ,5000, "Sistemas" );
+        //Uso de la clase padre
+        Empleado empleado = new Empleado("Juan", 5000);
+        System.out.println("empleado = " + empleado.obtenerDetalles());
+        Gerente gerente = new Gerente("Karla" ,10000, "Sistemas Artificiales" );
         gerente.obtenerDetalles();
         System.out.println("gerente = " + gerente.obtenerDetalles());
+        
+        //Esto es despues para el polimorfismo
+        System.out.println("");
+        imprimir(empleado);
+        System.out.println("");
+        imprimir(gerente);
     }
+    
+    public static void imprimir (Empleado empleado){
+        System.out.println("empleado = " + empleado.obtenerDetalles());
+    }
+    
 }
