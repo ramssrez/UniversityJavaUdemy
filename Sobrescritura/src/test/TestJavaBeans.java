42b854247235a626b36c181a0eacd8114e709b03
ramssrez <ramssrez@gmail.com>
2021-01-05 19:41:28 -0600

    Creacion de la clase para pruebas

---
+++ Sobrescritura/src/test/TestJavaBeans.java
@@ -0,0 +1,10 @@
+package test;
+
+import domain.Persona;
+
+public class TestJavaBeans {
+    public static void main(String[] args) {
+        //Prueba para javaBeans
+        Persona persona = new Persona();
+    }
+}
