fdc2b14046445f3e407c57fdb4e8a7778e018c35
ramssrez <ramssrez@gmail.com>
2021-01-05 19:46:25 -0600

    Uso de JavaBeans

--- Sobrescritura/src/test/TestJavaBeans.java
+++ Sobrescritura/src/test/TestJavaBeans.java
@@ -5,6 +5,10 @@ import domain.Persona;
 public class TestJavaBeans {
     public static void main(String[] args) {
         //Prueba para javaBeans
+        //Se usa la Serizalizable para que la clase o objeto pueda pasar a otro servidor
         Persona persona = new Persona();
+        persona.setNombre("Juan");
+        persona.setApellido("Perez");
+        System.out.println("persona = " + persona.toString());
     }
 }
