b77201dce7537ef92005d835908d274017ec544b
ramssrez <ramssrez@gmail.com>
2021-01-05 19:39:11 -0600

    Creacion de las clases de JAva Beans

--- Sobrescritura/src/domain/Persona.java
+++ Sobrescritura/src/domain/Persona.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domain;
 
-/**
- *
- * @author ramssrez
- */
-public class Persona {
+import java.io.Serializable;
+
+//Uso del concepto de Java Beans en donde se usa la interface Serizalizable
+public class Persona implements Serializable{
+    private String nombre;
+    private String apellido;
     
+    //Para freamwork es necesario de hacer uso de un constructor vacío
+    public Persona(){
+        
+    }
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + '}';
+    }
+        
 }
