9d8585bc197dedfe7cd2598fa2b3f07a3f7850c0
ramssrez <ramssrez@gmail.com>
2021-01-13 20:19:27 -0600

    Uso de set como coleccion

--- Sobrescritura/src/test/TestColecciones.java
+++ Sobrescritura/src/test/TestColecciones.java
@@ -1,12 +1,14 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 //Uso de API Colleciones para Java
 public class TestColecciones {
+
     public static void main(String[] args) {
         //Uso de Arraylista para una lista
         List miLista = new ArrayList();
@@ -17,19 +19,17 @@ public class TestColecciones {
         miLista.add("Miercoles");
         miLista.add("Jueves");
         miLista.add("Viernes");
+        miLista.add("Viernes");
         miLista.add("Sabado");
-        miLista.add("Domingo");  
-        
+        miLista.add("Domingo");
+
         //Uso del foreach para poder observar como es la organización de los elmenetos que se han utilizado
-        for (Object elemento : miLista) {
-            System.out.println("elemento: " + elemento);
-        }
-        
+//        for (Object elemento : miLista) {
+//            System.out.println("elemento: " + elemento);
+//        }
         System.out.println("");
         //uso de operaciones tipo landa o flecha
-        miLista.forEach(elemento -> {
-            System.out.println("elemento: " + elemento);
-        });
+        imprimir(miLista);
         
         System.out.println("");
         //Uso de set, es similar al array pero con la diferencia que no guarda un orden
@@ -39,11 +39,17 @@ public class TestColecciones {
         miSet.add("Miercoles");
         miSet.add("Jueves");
         miSet.add("Viernes");
-                
+        //No se pueden repetir valores repetidos en el elementos set
+        miSet.add("Viernes");
+        imprimir(miSet);
         
-        miSet.forEach(elemento ->{
-                System.out.println("elemento: " + elemento);
-        });
     }
-           
+    
+    //Método que imprime las colecciiones que estamos analizando
+    public static void imprimir(Collection collection) {
+        collection.forEach(elemento -> {
+            System.out.println("elemento = " + elemento);
+        });
+    }    
+    
 }
