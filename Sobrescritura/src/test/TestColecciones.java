0c05b8eae1a974cfe12bf6215b164729093d38d4
ramssrez <ramssrez@gmail.com>
2021-01-29 15:38:06 -0600

    Uso de genericos para las clases genericas

--- Sobrescritura/src/test/TestColecciones.java
+++ Sobrescritura/src/test/TestColecciones.java
@@ -13,7 +13,8 @@ public class TestColecciones {
 
     public static void main(String[] args) {
         //Uso de Arraylista para una lista
-        List miLista = new ArrayList();
+        //Modificacion de la lista para que se pueda agregar tipos genericos
+        List <String> miLista = new ArrayList<>();
         //Una lista mantienen el orden de los elementos conforme se van agregando
         miLista.add("Lunes");
         miLista.add("Martes");
@@ -25,6 +26,11 @@ public class TestColecciones {
         miLista.add("Viernes");
         miLista.add("Sabado");
         miLista.add("Domingo");
+        
+        String elementoPrueba = miLista.get(0);
+        System.out.println("elementoPrueba = " + elementoPrueba);
+        
+        
 
         //Uso del foreach para poder observar como es la organización de los elmenetos que se han utilizado
 //        for (Object elemento : miLista) {
@@ -66,7 +72,7 @@ public class TestColecciones {
     }
     
     //Método que imprime las colecciiones que estamos analizando
-    public static void imprimir(Collection collection) {
+    public static void imprimir(Collection<String> collection) {
         collection.forEach(elemento -> {
             System.out.println("elemento = " + elemento);
         });
