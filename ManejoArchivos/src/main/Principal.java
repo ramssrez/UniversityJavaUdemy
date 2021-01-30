5d01cad0c71ace75a070ecd042b591855ce10c55
ramssrez <ramssrez@gmail.com>
2021-01-29 18:36:29 -0600

    Creacion de ventana con java con la paqueteria swing

--- ManejoArchivos/src/main/Principal.java
+++ ManejoArchivos/src/main/Principal.java
@@ -1,5 +1,24 @@
 package main;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+
 public class Principal {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Esta es la clase principal");
+        JFrame frame = new JFrame("Ejemlo de eventos");
+        frame.setLocation(100, 100);
+        frame.setVisible(true);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.out.println("Cerrado de ventana");
+                System.exit(0);
+            }
+
+        });
+
+    }
 }
