e0cfe2742b1080051fbe7272c3aea6578a691cb7
ramssrez <ramssrez@gmail.com>
2021-02-04 21:18:50 -0600

    Creacion del metodo buscar

--- CatalogoPeliculas/src/mx/com/gm/peliculas/datos/AccesoDatosImplemetation.java
+++ CatalogoPeliculas/src/mx/com/gm/peliculas/datos/AccesoDatosImplemetation.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import mx.com.gm.peliculas.domain.Pelicula;
@@ -26,7 +28,7 @@ public class AccesoDatosImplemetation implements IAccesoDatos {
             var entrada = new BufferedReader(new FileReader(archivo));
             String linea = null;
             linea = entrada.readLine();
-            while(linea!= null){
+            while (linea != null) {
                 var pelicula = new Pelicula(linea);
                 peliculas.add(pelicula);
                 linea = entrada.readLine();
@@ -44,10 +46,44 @@ public class AccesoDatosImplemetation implements IAccesoDatos {
 
     @Override
     public void escribir(Pelicula pelicula, String nombreRecurso, boolean anexar) throws EscrituraDatosEx {
+        var archivo = new File(nombreRecurso);
+        try {
+            var salida = new PrintWriter(new FileWriter(archivo, anexar));
+            salida.println(pelicula.toString());
+            salida.close();
+            System.out.println("Se ha escrito informaión al archivo: " + pelicula);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new EscrituraDatosEx("Exepcion al listareliculas: " + ex.getMessage());
+        }
     }
 
     @Override
     public String buscar(String nombreRecurso, String buscar) throws LecturaDatosEx {
+        var archivo = new File(nombreRecurso);
+        String resultado = null;
+        try {
+            var entrada = new BufferedReader(new FileReader(archivo));
+            String linea = null;
+            linea = entrada.readLine();
+            var indice = 1;
+            while (linea !=null) {   
+                if (buscar !=null &&buscar.equalsIgnoreCase(linea)) {
+                    resultado = "Pelicula " + linea + " encontrada en el indice: " + indice;
+                    break;
+                }
+                linea= entrada.readLine();
+                indice++;
+            }
+            entrada.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            throw new LecturaDatosEx("Excepcion al buscar peliculas: " + ex.getMessage());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new LecturaDatosEx("Excepcion al buscar peliculas: " + ex.getMessage());
+        }
+        return resultado;
     }
 
     @Override
