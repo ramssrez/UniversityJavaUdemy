7fbb2312017f595cd22f81cf4153b7e155bada27
ramssrez <ramssrez@gmail.com>
2021-02-09 07:02:18 -0600

    Creacion de los metodos de la interface

--- CatalogoPeliculas/src/mx/com/gm/pelicuas/servicio/ICatalogoPeliculas.java
+++ CatalogoPeliculas/src/mx/com/gm/pelicuas/servicio/ICatalogoPeliculas.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mx.com.gm.pelicuas.servicio;
 
-/**
- *
- * @author ramssrez
- */
 public interface ICatalogoPeliculas {
+
+    String NOMBRE_RECURSO = "Peliculas.txt";
+
+    void agregarPelicula(String nombrePelicula);
     
+    void listarPeliculas();
+    
+    void buscarPelicula(String buscar);
+    
+    void iniciarCatalogoPeiulas();
 }
