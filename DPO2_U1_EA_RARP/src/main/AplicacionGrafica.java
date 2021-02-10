6ad5b1c8b1d6d8a37375fe6ebf10efd2bb8bff79
ramssrez <ramssrez@gmail.com>
2021-02-10 11:57:30 -0600

    uso de botones y radi buttons

--- DPO2_U1_EA_RARP/src/main/AplicacionGrafica.java
+++ DPO2_U1_EA_RARP/src/main/AplicacionGrafica.java
@@ -26,6 +26,26 @@ public class AplicacionGrafica extends JFrame {
         var textField = new JTextField();
         textField.setBounds(43, 67, 86, 26);
         contentPanel.add(textField);
+        //Uso de boton en nuestro sistema
+        var button = new JButton("Pulsar");
+        button.setBounds(43, 133, 89, 23);
+        button.setBackground(Color.red);
+        contentPanel.add(button);
+        //Uso de radiobutton con su respectivo adatador
+        var rdButton = new JRadioButton("Opción 1", true);
+        var rdButton1 = new JRadioButton("Opción 2", false);
+        var rdButton2 = new JRadioButton("Opción 3", false);
+        rdButton.setBounds(43, 194, 109, 23);
+        rdButton1.setBounds(43, 220, 109, 23);
+        rdButton2.setBounds(43, 246, 109, 23);
+        contentPanel.add(rdButton);
+        contentPanel.add(rdButton1);
+        contentPanel.add(rdButton2);
+        
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(rdButton);
+        buttonGroup.add(rdButton1);
+        buttonGroup.add(rdButton2);
     }
 
     public static void main(String[] args) {
