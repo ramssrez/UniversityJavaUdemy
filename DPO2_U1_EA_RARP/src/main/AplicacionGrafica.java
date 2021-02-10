fc4d18e01cb700155e7b8df39ede8b55a2afb2e8
ramssrez <ramssrez@gmail.com>
2021-02-10 11:36:12 -0600

    uso de textField

--- DPO2_U1_EA_RARP/src/main/AplicacionGrafica.java
+++ DPO2_U1_EA_RARP/src/main/AplicacionGrafica.java
@@ -1,27 +1,35 @@
 package main;
 
+import java.awt.Color;
 import javax.swing.*;
 
-public class AplicacionGrafica  extends JFrame{
+public class AplicacionGrafica extends JFrame {
+
     private JPanel contentPanel;
-    public AplicacionGrafica(){
+
+    public AplicacionGrafica() {
         setTitle("Título de la ventana");
-        setBounds(400,200,700,500);
+        setBounds(400, 200, 700, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
         contentPanel = new JPanel();
         contentPanel.setLayout(null);
         setContentPane(contentPanel);
-        
+
         //Asignación de una pantalla
-        JLabel etiqueta =new JLabel("Hola mundo desde interfaz gráfica");
-        etiqueta.setBounds(60, 20, 150, 20);
+        //Etiqueta
+        JLabel etiqueta = new JLabel("¡Hola mundo!");
+        etiqueta.setBounds(60, 20, 100, 20);
         contentPanel.add(etiqueta);
-        
+
+        //Campo de texto
+        var textField = new JTextField();
+        textField.setBounds(43, 67, 86, 26);
+        contentPanel.add(textField);
     }
-   
+
     public static void main(String[] args) {
         AplicacionGrafica ventana = new AplicacionGrafica();
     }
-    
+
 }
