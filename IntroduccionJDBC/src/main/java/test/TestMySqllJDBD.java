c23971e81f4c94d80d3a7c1b4965b4f96819d117
ramssrez <ramssrez@gmail.com>
2021-02-11 14:34:35 -0600

    conexion con la base de datos

--- IntroduccionJDBC/src/main/java/test/TestMySqllJDBD.java
+++ IntroduccionJDBC/src/main/java/test/TestMySqllJDBD.java
@@ -1,17 +1,20 @@
 package test;
 
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class TestMySqllJDBD {
+
     public static void main(String[] args) {
         //Cadena de conexion en la base de datos
         String url = "jdbc:mysql://localhost:3307/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
         try {
             //Para aplicaciones web es requerida para poder conectarse a una base de datos
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(url,"root","admin");
+            Connection connection = DriverManager.getConnection(url, "root", "admin");
+            Statement statement = connection.createStatement();
+            var sql = "SELECT idpersona, nombre,apellido,email,telefono FROM persona";
+            ResultSet resultSet = statement.executeQuery(sql);
+            
         } catch (ClassNotFoundException ex) {
             ex.printStackTrace(System.out);
             System.out.println("error ClassNotFoundException" + ex.getMessage());
