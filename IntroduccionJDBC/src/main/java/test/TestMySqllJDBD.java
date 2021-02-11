0287ffd6e4bb433fee73dfc129de271a1a500c82
ramssrez <ramssrez@gmail.com>
2021-02-11 14:05:53 -0600

    Creación de la cadena de conexion para mySQL

--- IntroduccionJDBC/src/main/java/test/TestMySqllJDBD.java
+++ IntroduccionJDBC/src/main/java/test/TestMySqllJDBD.java
@@ -1,5 +1,9 @@
 package test;
 
 public class TestMySqllJDBD {
-    
+    public static void main(String[] args) {
+        //Cadena de conexion en la base de datos
+        String url = "jdbc:mysql://localhost:3307/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        
+    }
 }
