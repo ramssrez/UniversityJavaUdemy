83172b3f11ed054fc4cf560c7fe909ac2cf03122
ramssrez <ramssrez@gmail.com>
2021-02-16 19:40:23 -0600

    Creacion proyecto de interfaz para evidencia de aprendizaje POgit status

---
+++ DPOII_U1_EA_RARP/src/main/Principal.java
@@ -0,0 +1,14 @@
+package main;
+
+import grafics.Login;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        var grafico = new Login();
+        grafico.setTitle("Inicio de sesión");
+        grafico.setVisible(true);
+
+    }
+
+}
