32c37f7844321cb5286fa599b1596e388ed82e9c
ramssrez <ramssrez@gmail.com>
2021-02-20 22:04:53 -0600

    Creación del metodo para selecciona en la base de datos

--- ManejoJDBC/src/main/java/datos/PersonaDAO.java
+++ ManejoJDBC/src/main/java/datos/PersonaDAO.java
@@ -1,5 +1,25 @@
 package datos;
 
+import domain.Persona;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonaDAO {
+    //Definir la sentecia SQL para poder ingresar a la base de datos
+    private static final String SQL_SELECT = "SELECT idPersona, nombre,apellido, email, telefono FROM persona";
     
+    //Creación del metodo que selecciona los objetos de tipo personaa
+    public List<Persona> seleccionar(){
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        Persona persona = null;
+        List<Persona> personas = new ArrayList<>();
+        return null;
+    }
+        
+
 }
