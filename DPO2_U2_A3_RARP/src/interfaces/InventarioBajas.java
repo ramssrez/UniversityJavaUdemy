9205f5a0125cf2df8cb5b4d8be5787b779cc9a02
ramssrez <ramssrez@gmail.com>
2021-03-04 16:05:32 -0600

    creación de la ventana de inventario bajas

--- DPO2_U2_A3_RARP/src/interfaces/InventarioBajas.java
+++ DPO2_U2_A3_RARP/src/interfaces/InventarioBajas.java
@@ -4,14 +4,14 @@ package interfaces;
  *
  * @author ramssrez
  */
-public class InventarioAltas extends javax.swing.JFrame {
+public class InventarioBajas extends javax.swing.JFrame {
 
     /**
      * Creates new form InventarioAltas
      */
-    public InventarioAltas() {
+    public InventarioBajas() {
         initComponents();
-        this.setTitle("Altas al inventario");
+        this.setTitle("Bajas del inventario");
         this.setLocationRelativeTo(null);
     }
 
@@ -358,20 +358,21 @@ public class InventarioAltas extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(InventarioAltas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InventarioBajas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(InventarioAltas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InventarioBajas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(InventarioAltas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InventarioBajas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(InventarioAltas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InventarioBajas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new InventarioAltas().setVisible(true);
+                new InventarioBajas().setVisible(true);
             }
         });
 
