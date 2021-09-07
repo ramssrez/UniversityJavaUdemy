65ceaf6281c25b5396cd63ce0a04c2804320756e
Raul Ramirez Perez <ramssrez@gmail.com>
2021-09-07 14:38:44 -0500

    Prueba para el nombre

--- BootCampJava/JS/ciclos.js
+++ BootCampJava/JS/ciclos.js
@@ -4,3 +4,11 @@ for(var i = 0; i < frutas.length ; i++){
     console.log("Fruta " + i + ": " +frutas[i]);
 }
 //esto es un comnentario
+//Esto es un cambio para el nombre
+/*
+sdfslddf
+asdfsadfjasdf
+asdfkasdf
+asdflasdf
+sjlkdf
+*/
