6bec04278d67104e11dcd33bd6f708029747679d
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-06 19:20:14 -0500

    Creación del menú

--- UnADM/universidadPOO3/DPO3_U1_A2_RARP/src/main/CrearArchivos.java
+++ UnADM/universidadPOO3/DPO3_U1_A2_RARP/src/main/CrearArchivos.java
@@ -9,12 +9,12 @@ package main;
  *
  * @author ramssrez
  */
-public class Menu extends javax.swing.JFrame {
+public class CrearArchivos extends javax.swing.JFrame {
 
     /**
-     * Creates new form Menu
+     * Creates new form CrearArchivos
      */
-    public Menu() {
+    public CrearArchivos() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class Menu extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CrearArchivos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CrearArchivos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CrearArchivos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CrearArchivos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Menu().setVisible(true);
+                new CrearArchivos().setVisible(true);
             }
         });
     }
