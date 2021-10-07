40bcbb3da66e3e21263ca7dc2f21d2b236c0fd67
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-06 19:05:10 -0500

    Creación de la primera unidad de Poo3

--- carpetaUdemy/fundamentosJava/Ventas/src/mx/com/gm/ventas/test/VentasTest.java
+++ carpetaUdemy/fundamentosJava/Ventas/src/mx/com/gm/ventas/test/VentasTest.java
@@ -7,6 +7,7 @@ public class VentasTest {
     public static void main(String[] args) {
         Producto producto1 = new Producto("Camisa", 50.00);
         Producto producto2 = new Producto("Pantaon", 100.00);
+        System.out.println("Esto es una prueba");
 
         Orden orden1 = new Orden();
         orden1.agregarProducto(producto1);
