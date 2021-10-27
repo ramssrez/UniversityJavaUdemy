4852444dcb5901b00bc4af2ec9ded2a4391b5ad2
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-27 10:23:08 -0500

    Uso de la sentencias para modficar campos de la tabla

--- UnADM/basesdedatos/DABD_U2_A1_RARP/modificar.sql
+++ UnADM/basesdedatos/DABD_U2_A1_RARP/modificar.sql
@@ -1,3 +1,7 @@
 /* Modificar nombre de una tabla*/
 ALTER TABLE vendedor RENAME TO cajero;
-ALTER TABLE millas RENAME TO kilometros
+ALTER TABLE millas RENAME TO kilometros;
+
+/*Cambio de nombre de columna y creación de nuevo campo */
+ALTER TABLE usuario CHANGE COLUMN edad fechanacimiento DATE NOT NULL;
+ALTER TABLE usuario ADD COLUMNS sexo CHAR(10) NOT NULL AFTER correo;
