ed09ea600c7dc1741b562b6858928e7169cf86a2
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-26 21:23:55 -0500

    Creación de la tabla sercvici

--- UnADM/basesdedatos/DABD_U2_A1_RARP/creartablas.sql
+++ UnADM/basesdedatos/DABD_U2_A1_RARP/creartablas.sql
@@ -2,3 +2,34 @@
 CREATE DATABASE es1921023412;
 USE es1921023412;
 
+/* Creación de la tabla Servicio */
+CREATE TABLE IF NOT EXISTS es1921023412.servicio (
+  idservicio INT NOT NULL AUTO_INCREMENT,
+  fecha DATE NOT NULL,
+  origen VARCHAR(20) NOT NULL,
+  destino VARCHAR(45) NOT NULL,
+  hora TIME NOT NULL,
+  clase VARCHAR(45) NOT NULL,
+  corrida VARCHAR(45) NOT NULL,
+  costototal VARCHAR(45) NOT NULL,
+  asiento INT NOT NULL,
+  numcomprobante INT NOT NULL,
+  vendedor VARCHAR(45) NOT NULL,
+  idcajero INT NOT NULL,
+  idusuario INT NOT NULL,
+  PRIMARY KEY (idservicio),
+  INDEX fk_servicio_vendedor_index (idcajero ASC) INVISIBLE,
+  INDEX fk_servicio_usuario_index (idusuario ASC) INVISIBLE,
+  CONSTRAINT fk_servicio_vendedor
+    FOREIGN KEY (idcajero)
+    REFERENCES es1921023412.vendedor (idcajero)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_servicio_usuario
+    FOREIGN KEY (idusuario)
+    REFERENCES es1921023412.Usuario (idusuario)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
