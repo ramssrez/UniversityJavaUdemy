db241333b4cc78f1a23f491a9e7366a312069feb
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-27 10:35:17 -0500

    Uso de check table para observar el estado de las tablas

--- UnADM/basesdedatos/DABD_U2_A1_RARP/modificar.sql
+++ UnADM/basesdedatos/DABD_U2_A1_RARP/modificar.sql
@@ -5,3 +5,10 @@ ALTER TABLE millas RENAME TO kilometros;
 /*Cambio de nombre de columna y creación de nuevo campo */
 ALTER TABLE usuario CHANGE COLUMN edad fechanacimiento DATE NOT NULL;
 ALTER TABLE usuario ADD COLUMNS sexo CHAR(10) NOT NULL AFTER correo;
+
+/* Verificar el estado de unas tablas */
+CHECK TABLE usuario;
+CHECK TABLE cajero;
+CHECK TABLE facturacion;
+CHECK TABLE kilometros;
+CHECK TABLE servicio;
