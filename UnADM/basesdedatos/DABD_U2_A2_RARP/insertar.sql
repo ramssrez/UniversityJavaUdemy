5570acc03e026526d59f295b66d4864c345caeaf
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-26 22:19:38 -0500

    Creación de las sentencias para insertar tablas a los usuarios

--- UnADM/basesdedatos/DABD_U2_A2_RARP/insertar.sql
+++ UnADM/basesdedatos/DABD_U2_A2_RARP/insertar.sql
@@ -1,1 +1,6 @@
+/* Insertar valores a la tabla de usuario */
 INSERT INTO usuario (nombre, apellido1, apellido2, fechanacimiento, correo, sexo) VALUES ('Jose', 'Martinez', 'Sanchez', '1969-12-01', 'jms@gmail.com', 'Masculino');
+INSERT INTO usuario (nombre, apellido1, apellido2, fechanacimiento, correo, sexo) VALUES ('Maria', 'Lopez', 'Reyna', '1980-05-13', 'mlr@gmail.com', 'Femenino');
+INSERT INTO usuario (nombre, apellido1, apellido2, fechanacimiento, correo, sexo) VALUES ('Enrique', 'Jimenez', 'Rodriguez', '1970-10-12', 'ejr@gmail.com', 'Masculino');
+INSERT INTO usuario (nombre, apellido1, apellido2, fechanacimiento, correo, sexo) VALUES ('Elizabeth', 'Nuñez', 'Fernandez', '1985-01-25', 'enf@gmail.com', 'Femenino');
+INSERT INTO usuario (nombre, apellido1, apellido2, fechanacimiento, correo, sexo) VALUES ('Raúl', 'Ramírez', 'Pérez', '1992-06-27', 'rrp@gmail.com', 'Masculino');
