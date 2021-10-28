3e0b2147bea3c7e0c3f52f82785519908cfb7097
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-27 20:54:42 -0500

    Uso de inner join para obtener los datos de la sentencia

--- UnADM/basesdedatos/DABD_U2_A2_RARP/seleccionar.sql
+++ UnADM/basesdedatos/DABD_U2_A2_RARP/seleccionar.sql
@@ -7,3 +7,9 @@ SELECT nombre, apellido1, apellido2, correo,  DATE_FORMAT( fechanacimiento,'%d-%
 
 /* Selecciona el número de usuarios registrados agrupados por sexo. */
 SELECT * FROM usuario ORDER BY sexo;
+
+/* Muestra la lista Facturación de los usuarios que han realizado este servicio
+ y que contenga los siguientes 
+ datos: id del usuario, nombre completo del usuario, fecha y autorización. */
+ SELECT usuario.idusuario, nombre, apellido1, apellido2,fecha, autorizacion FROM usuario
+ INNER JOIN facturacion WHERE usuario.idusuario = facturacion.idusuario;
