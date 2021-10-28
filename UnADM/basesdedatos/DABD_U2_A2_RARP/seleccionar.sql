827c8fcf6ee7413c199d7e903e42fc3ecd93164f
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-27 21:02:09 -0500

    Integración de las sentencias a realizar

--- UnADM/basesdedatos/DABD_U2_A2_RARP/seleccionar.sql
+++ UnADM/basesdedatos/DABD_U2_A2_RARP/seleccionar.sql
@@ -9,7 +9,32 @@ SELECT nombre, apellido1, apellido2, correo,  DATE_FORMAT( fechanacimiento,'%d-%
 SELECT * FROM usuario ORDER BY sexo;
 
 /* Muestra la lista Facturación de los usuarios que han realizado este servicio
- y que contenga los siguientes 
- datos: id del usuario, nombre completo del usuario, fecha y autorización. */
+ y que contenga los siguientes datos: id del usuario, nombre completo del usuario, fecha y autorización. */
  SELECT usuario.idusuario, nombre, apellido1, apellido2,fecha, autorizacion FROM usuario
  INNER JOIN facturacion WHERE usuario.idusuario = facturacion.idusuario;
+
+/* Elimina los datos de usuario que pertenece a tu nombre. (utilizar subconsulta). */
+ DELETE FROM usuario WHERE (idusuario = 6);
+ SELECT * FROM usuario;
+
+
+/*  Muestra los Usuarios que recibirán descuentos por los kilómetros acumulados.*/
+
+
+/* Selecciona al último usuario registrado en la base de datos. */
+
+
+/* Actualiza la fecha de nacimiento de un usuario registrado a 03/06/1976. */
+
+
+/* Selecciona los usuarios registrados con su edad (calcular a partir de la fecha de nacimiento almacenada). */
+
+
+/* Selecciona los usuarios que tengan un descuento cuyo sexo sea femenino y sean mayores de 35 años y menores a 50 años. */
+
+
+/* Ordena los usuarios de manera ascendente respecto de su Apellido1. */
+
+
+/* Selecciona el nombre y apellido1 de los usuarios, así como el asiento, fecha,origen, destino, hora y clase del servicio. */
+
