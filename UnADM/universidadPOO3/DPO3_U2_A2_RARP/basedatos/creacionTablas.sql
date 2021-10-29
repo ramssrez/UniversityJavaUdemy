a69c6982e86ecf366690b23ee50bf6a16251ff3a
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-28 20:37:39 -0500

    Creación de las sentencias para crear las tablas

--- UnADM/universidadPOO3/DPO3_U2_A2_RARP/basedatos/creacionTablas.sql
+++ UnADM/universidadPOO3/DPO3_U2_A2_RARP/basedatos/creacionTablas.sql
@@ -1,37 +1,33 @@
+/* Creación de la base de datos */
 CREATE DATABASE la_estancia;
+/* Uso de las tablas */
 USE la_estancia;
+
+/*Creación de la tabla habitaciones*/
 CREATE TABLE habitaciones(
     id_habitacion INT NOT NULL AUTO_INCREMENT,
     tipo_habitación VARCHAR(25) NOT NULL,
     no_habitacion INT NOT NULL,
     estatus VARCHAR(10) NOT NULL,
-    costo FLOAT,
+    costo FLOAT NOT NULL,
     PRIMARY KEY(id_habitacion) 
 );
 
+/* Creación de la tabla reservaciones*/
 CREATE TABLE reservaciones(
     id_reservacion INT NOT NULL AUTO_INCREMENT,
     fecha_entrada DATE NOT NULL,
     fecha_salida DATE NOT NULL,
     dias VARCHAR(15) NOT NULL,
     id_habitacion INT NOT NULL,
-    PRIMARY KEY(id_reservacion)
+    PRIMARY KEY(id_reservacion),
+    INDEX fk_reservacion_habitacion_idx (id_habitacion ASC) VISIBLE,
+    CONSTRAINT fk_reservacion_habitacion
+    FOREIGN KEY(id_habitacion)
+    REFERENCES la_estancia.habitaciones(id_habitacion)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
-
-
-
-Tabla RESERVACIONES
-id_reservacion
-fecha_entrada
-fecha_salida
-dias (se calculará con las columnas de fecha_entrada y fecha_salida)
-id_habitacion
-
-
-Tabla HABITACIONES
-id_habitacion
-tipo_habitación (sencilla, doble, triple y cuádruple)
-no_habitacion
-estatus (ocupado, disponible)
-costo (costo de la habitación por noche).
+/* Muestra de la tablas creadas */
+SHOW TABLES;
