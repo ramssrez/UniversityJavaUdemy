0144d34a534cd75b25da0b07f63692d6beac9d06
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-29 18:32:09 -0500

    Integración de los métodos getters y setters

--- UnADM/universidadPOO3/DPO3_U2_A2_RARP/Estancia/src/objetos/Habitacion.java
+++ UnADM/universidadPOO3/DPO3_U2_A2_RARP/Estancia/src/objetos/Habitacion.java
@@ -13,4 +13,46 @@ public class Habitacion {
     private int numeroHabitacion;
     private String status;
     private int costoHabitacion;
+
+    public int getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public String getTipoHabitacion() {
+        return tipoHabitacion;
+    }
+
+    public void setTipoHabitacion(String tipoHabitacion) {
+        this.tipoHabitacion = tipoHabitacion;
+    }
+
+    public int getNumeroHabitacion() {
+        return numeroHabitacion;
+    }
+
+    public void setNumeroHabitacion(int numeroHabitacion) {
+        this.numeroHabitacion = numeroHabitacion;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getCostoHabitacion() {
+        return costoHabitacion;
+    }
+
+    public void setCostoHabitacion(int costoHabitacion) {
+        this.costoHabitacion = costoHabitacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitacion{" + "idHabitacion=" + idHabitacion + ", tipoHabitacion=" + tipoHabitacion + ", numeroHabitacion=" + numeroHabitacion + ", status=" + status + ", costoHabitacion=" + costoHabitacion + '}';
+    }
+    
 }
