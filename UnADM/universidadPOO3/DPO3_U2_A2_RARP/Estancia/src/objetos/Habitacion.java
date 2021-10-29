02ec89db68b33031b31595b7131a9dfbabc2ff96
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-29 18:25:40 -0500

    Integración de los atributos

--- UnADM/universidadPOO3/DPO3_U2_A2_RARP/Estancia/src/objetos/Habitacion.java
+++ UnADM/universidadPOO3/DPO3_U2_A2_RARP/Estancia/src/objetos/Habitacion.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Código elaborado por: Raúl Ramírez Pérez
  */
 package objetos;
 
@@ -10,5 +8,9 @@ package objetos;
  * @author ramssrez
  */
 public class Habitacion {
-    
+    private int idHabitacion;
+    private String tipoHabitacion;
+    private int numeroHabitacion;
+    private String status;
+    private int costoHabitacion;
 }
