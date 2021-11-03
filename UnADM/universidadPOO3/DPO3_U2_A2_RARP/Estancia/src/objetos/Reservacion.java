6267cdd90828a6601e4441dd5873461ce76fafa0
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-02 18:47:52 -0600

    Integración del modelo de reservacion

--- UnADM/universidadPOO3/DPO3_U2_A2_RARP/Estancia/src/objetos/Reservacion.java
+++ UnADM/universidadPOO3/DPO3_U2_A2_RARP/Estancia/src/objetos/Reservacion.java
@@ -1,14 +1,41 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Código elaborado por: Raúl Ramírez Pérez
  */
 package objetos;
+import java.sql.Date;
 
 /**
  *
  * @author ramssrez
  */
 public class Reservacion {
+    private Date fechaEntrada;
+    private Date fechaSalida;
+    private int idHabitacion;
+
+    public Date getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(Date fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public int getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public void setIdHabitacion(int idHabitacion) {
+        this.idHabitacion = idHabitacion;
+    }
+    
     
 }
