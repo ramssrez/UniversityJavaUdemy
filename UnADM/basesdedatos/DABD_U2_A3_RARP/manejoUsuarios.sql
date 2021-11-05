a06cb478a7d160851550390323a4dd47db5c149e
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-05 15:50:47 -0600

    Creacion del archivo para crear documentos

---
+++ UnADM/basesdedatos/DABD_U2_A3_RARP/manejoUsuarios.sql
@@ -0,0 +1,5 @@
+/*Creación de un nuevo usuario profesora  */
+CREATE USER 'AnaKarenCamacho'@'localhost' IDENTIFIED BY 'passAKC';
+
+/* Creación de mi usuario */
+CREATE USER 'RaulRamirez'@'localhost' IDENTIFIED BY 'passRR';
