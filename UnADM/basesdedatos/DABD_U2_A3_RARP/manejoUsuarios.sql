3c2465bfe8108f0947f7064f84c2ea3f4e91726a
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-05 16:32:31 -0600

    integración de las consultas para asignar permisos

--- UnADM/basesdedatos/DABD_U2_A3_RARP/manejoUsuarios.sql
+++ UnADM/basesdedatos/DABD_U2_A3_RARP/manejoUsuarios.sql
@@ -1,5 +1,14 @@
 /*Creación de un nuevo usuario profesora  */
 CREATE USER 'AnaKarenCamacho'@'localhost' IDENTIFIED BY 'passAKC';
 
+/*Asignar privilegios */
+GRANT ALL PRIVILEGES ON * . * TO 'AnaKarenCamacho'@'localhost';
+
+/* Aseguramiento para refrecar todo los permisos */
+FLUSH PRIVILEGES;
+
+
 /* Creación de mi usuario */
 CREATE USER 'RaulRamirez'@'localhost' IDENTIFIED BY 'passRR';
+
+GRANT INSERT SELECT ON *.* to 'RaulRamirez'@'localhost':
