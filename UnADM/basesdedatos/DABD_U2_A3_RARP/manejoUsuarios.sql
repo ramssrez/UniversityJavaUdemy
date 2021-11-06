5de868834c1f0e90d87c7b3f56f37955d5f22fd6
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-06 17:15:55 -0600

    Fianlización del ejericicio de la actividad de la base de datos

--- UnADM/basesdedatos/DABD_U2_A3_RARP/manejoUsuarios.sql
+++ UnADM/basesdedatos/DABD_U2_A3_RARP/manejoUsuarios.sql
@@ -26,6 +26,21 @@ SHOW GRANTS;
 /* Modificacion de la tabla usuario */
 UPDATE es1921023412.usuario SET fechanacimiento = '1970-12-01' WHERE (idusuario = '1');
 
-
+/* Sentencia de busqueda */
  SELECT nombre, apellido1, apellido2,fechanacimiento,razonsocial, direccion,fecha, autorizacion FROM usuario
  INNER JOIN facturacion WHERE usuario.idusuario = facturacion.idusuario;
+
+/* Creación del rol */
+ CREATE ROLE 'MI_ROL';
+ GRANT UPDATE, DELETE ON es1921023412.* TO MI_ROL; 
+
+/* Muestra todos los usuarios */
+ select User from mysql.user;
+
+ GRANT MI_ROL TO 'RaulRamirez'@'localhost';
+ SHOW GRANTS FOR 'RaulRamirez'@'localhost';
+
+/* Muestra los permisos con un rol */
+ SHOW GRANTS FOR 'RaulRamirez'@'localhost' USING 'MI_ROL';
+
+ REVOKE SELECT ON es1921023412.* FROM 'RaulRamirez'@'localhost';
