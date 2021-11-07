9e63b8c13c5219f404c89793069f0ea9419e0766
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-07 17:26:20 -0600

    insertar datos a las tablas refugios y perros

--- UnADM/basesdedatos/DABD_U2_EA_RARP/insertar.sql
+++ UnADM/basesdedatos/DABD_U2_EA_RARP/insertar.sql
@@ -31,10 +31,24 @@ INSERT INTO lomitos.raza (descripcion) VALUES ('Hunteraway');
 INSERT INTO lomitos.raza (descripcion) VALUES ('Mixto');
 
 /* insertar datos en la tabla direcciones */
-INSERT INTO lomitos.direcciones (calle, colonia, municipio, numero, id_estado) VALUES ('Revolución', 'Sanctorum', 'Cuautlacingo', '12', '3');
 INSERT INTO lomitos.direcciones (calle, colonia, municipio, numero, id_estado) VALUES ('Reforma', 'Centro', '222', 'Cuautemoc',  '1');
 INSERT INTO lomitos.direcciones (calle, colonia, municipio, numero, id_estado) VALUES ('Allende', 'La Conchita', 'Texcoco', '205', '2');
-INSERT INTO lomitos.direcciones (calle, colonia, municipio, numero, id_estado) VALUES ('San Agustin', 'Centro', 'Texcoco', '27', '2');
+INSERT INTO lomitos.direcciones (calle, colonia, municipio, numero, id_estado) VALUES ('Revolución', 'Sanctorum', 'Cuautlacingo', '12', '3');
 INSERT INTO lomitos.direcciones (calle, colonia, municipio, numero, id_estado) VALUES ('Lazaron Cardenas', 'Buenos Aires', 'Benito Juarez', '205', '1');
+INSERT INTO lomitos.direcciones (calle, colonia, municipio, numero, id_estado) VALUES ('San Agustin', 'Centro', 'Texcoco', '27', '2');
 INSERT INTO lomitos.direcciones (calle, colonia, municipio, numero, id_estado) VALUES ('Poniente 15', 'Centro', 'Puebla', '15', '3');
 
+/* Insertar datos en la tabla adoptantes */
+INSERT INTO lomitos.adoptantes (nombre, apellido_paterno, apellido_materno, fecha_nacimieno, correo, id_direccion) VALUES ('Pedro', 'Juarez', 'Sanchez', '1996-05-06', 'pjs@gmail.conm', '4');
+INSERT INTO lomitos.adoptantes (nombre, apellido_paterno, apellido_materno, fecha_nacimieno, correo, id_direccion) VALUES ('Ernesto', 'Labastida', 'Figueroa', '1990-01-30', 'elf@gmail.com', '5');
+INSERT INTO lomitos.adoptantes (nombre, apellido_paterno, apellido_materno, fecha_nacimieno, correo, id_direccion) VALUES ('Frida', 'Rodriguez', 'Martinez', '1980-06-28', 'frm@gmail.com', '6');
+
+/* Insertar datos en la tabla refugios */
+INSERT INTO lomitos.refugios (nombre, id_direccion) VALUES ('La esperanza CDMX', '1');
+INSERT INTO lomitos.refugios (nombre, id_direccion) VALUES ('La esperanza  EDOMEX', '2');
+INSERT INTO lomitos.refugios (nombre, id_direccion) VALUES ('La esperanza Puebla', '3');
+
+/* Insertar datos en la tabla perros */
+INSERT INTO lomitos.perros (nombre, id_raza, id_sexo, id_size, id_color, id_adoptante, id_refugio) VALUES ('Kira', '5', '2', '2', '4', '1', '1');
+INSERT INTO lomitos.perros (nombre, id_raza, id_sexo, id_size, id_color, id_adoptante, id_refugio) VALUES ('Morena', '3', '2', '1', '1', '2', '2');
+INSERT INTO lomitos.perros (nombre, id_raza, id_sexo, id_size, id_color, id_adoptante, id_refugio) VALUES ('Sam', '2', '1', '3', '6', '3', '3');
