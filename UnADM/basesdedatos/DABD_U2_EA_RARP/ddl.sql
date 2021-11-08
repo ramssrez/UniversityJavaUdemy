b322c5ec647237fbc83c3f00439a30604444417c
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-07 18:40:27 -0600

    Eliminaicon de relaciones

--- UnADM/basesdedatos/DABD_U2_EA_RARP/ddl.sql
+++ UnADM/basesdedatos/DABD_U2_EA_RARP/ddl.sql
@@ -1,5 +1,7 @@
+/* Cambio de nombre de la llave foranea */
 ALTER TABLE lomitos.direcciones 
 DROP FOREIGN KEY fk_direccion_estado;
+
 ALTER TABLE lomitos.direcciones 
 CHANGE COLUMN id_estado idEstado INT NOT NULL ;
 
@@ -7,3 +9,11 @@ ALTER TABLE lomitos.direcciones
 ADD CONSTRAINT fk_direccion_estado
   FOREIGN KEY (idEstado)
   REFERENCES lomitos.estado (id);
+
+  /* Eliminación de relaciones */
+ALTER TABLE lomitos.perros 
+DROP FOREIGN KEY fk_perros_sexo;
+
+ALTER TABLE lomitos.perros 
+DROP INDEX fk_perros_sexo_idx ;
+;
