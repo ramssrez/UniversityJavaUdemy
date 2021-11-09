ed7d968f8ba0e5ec083752d574693666b746bb3b
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-08 21:01:25 -0600

    Creación de la sentencia para insertar, asi como el metodo asociado

--- UnADM/universidadPOO3/DPO3_U2_EA_RARP/maids/src/database/EmpleadoDAO.java
+++ UnADM/universidadPOO3/DPO3_U2_EA_RARP/maids/src/database/EmpleadoDAO.java
@@ -21,6 +21,9 @@ public class EmpleadoDAO {
     //Declaración de la sentencia a realizar para selecionar la información de los empleados
     private static final String SQL_SELECT = "SELECT id_comision, fecha, nombre_trabajador, nombre_cliente, tiempo_trabajado, comision_obtenida, "
             + "descuentos_realizados, sueldototal FROM empleados;";
+    
+    private static final String SQL_INSERT = "INSERT INTO empleados (fecha, nombre_trabajador, nombre_cliente, tiempo_trabajado, comision_obtenida, descuentos_realizados, sueldototal) "
+            + "VALUES (?,?,?,?,?,?,?);";
 
     //Método que obtiene la lista de las habitaciones que hay en la base de datos
     public List<Empleado> seleccionarLista() {
@@ -76,6 +79,48 @@ public class EmpleadoDAO {
         }
         //Se envia el objeto empleado que se ha generado
         return listaEmpleados;
+    }
+    
+    public int insertar(Reservacion reservacion) {
+        //Declaración de las variables necesrias para poder realizar la conexion a la base de datos.
+        //Declaración del objeto del canal de conexión
+        Connection conn = null;
+        //Declaración del objetos de sentencias
+        PreparedStatement preparedStatement = null;
+        //Delcaración de la variable que verifica si se ha hecho una modificación al registro
+        int registros = 0;
+        //Bloque try/catch para las conexiones
+        try {
+            //Declaración del canal de coneción
+            conn = ConexionDB.getConnection();
+            //Envio de sentencias SQL para insertar datos a la base de datos
+            preparedStatement = conn.prepareStatement(SQL_INSERT);
+            //Envio de los parametros que se han seleccionado para poder realizar la insersión de datos
+            //Insersión de la la variable de tipo DateSql para la base de datos
+            preparedStatement.setDate(1, reservacion.getFechaEntrada());
+            preparedStatement.setDate(2, reservacion.getFechaSalida());
+            preparedStatement.setString(3, String.valueOf(reservacion.getIdHabitacion()));
+            preparedStatement.setString(4, String.valueOf(reservacion.getDias()));
+            preparedStatement.setString(5, String.valueOf(reservacion.getCostoTotal()));
+            //Sentencia para que se realice el alza de los datos
+            registros = preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace(System.out);
+            registros = 0;
+        } finally {
+            try {
+                //Cierre de la sentecia enviada
+                ConexionDB.close(preparedStatement);
+                //Cierre del canal de conexión
+                ConexionDB.close(conn);
+            } catch (SQLException ex) {
+                System.out.println("Error: " + ex.getMessage());
+                ex.printStackTrace(System.out);
+            }
+        }
+        //Retorno de registros afectados
+        return registros;
     }
 
 }
