00be3d81a7e414c987e11600e1d7bd9b3bd6e431
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-08 19:40:32 -0600

    Creción de los scripts para insertar y seleccionar en la base de datos

---
+++ UnADM/universidadPOO3/DPO3_U2_EA_RARP/basedatos/insertar.sql
@@ -0,0 +1,5 @@
+INSERT INTO bd_ea.empleados (fecha, nombre_trabajador, nombre_cliente, tiempo_trabajado, comision_obtenida, descuentos_realizados, sueldototal) VALUES ('2021-11-01', 'Jose Sanchez Hernandez', 'Ernesto Lopez Pérez', '480', '50', '10', '120');
+INSERT INTO bd_ea.empleados (fecha, nombre_trabajador, nombre_cliente, tiempo_trabajado, comision_obtenida, descuentos_realizados, sueldototal) VALUES ('2021-11-02', 'Alejandro Espejel Ramirez', 'Julio Cuevas Brito', '480', '50', '10', '120');
+
+/* Seleccionar datos */
+SELECT * FROM bd_ea.empleados;
