8eca74e7a928800422edd7d78547ce1eb12670b2
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-12 20:07:59 -0600

    Cración de los metodos para el menú

--- UnADM/universidadPOO3/DPO3_U2_EA_RARP/maids/src/interfaces/Inicio.java
+++ UnADM/universidadPOO3/DPO3_U2_EA_RARP/maids/src/interfaces/Inicio.java
@@ -5,9 +5,6 @@
  */
 package interfaces;
 
-import interfaces.AltasInventario;
-import interfaces.AltasPersonal;
-
 /**
  *
  * @author ramssrez
@@ -37,8 +34,8 @@ public class Inicio extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenu2 = new javax.swing.JMenu();
+        jIemAsigar = new javax.swing.JMenuItem();
+        jItemReporte = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -46,29 +43,29 @@ public class Inicio extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel1.setText("Selecciona una opción del menú");
 
-        jMenu1.setText("Personal");
+        jMenu1.setText("Asignar Trabajo");
 
-        jMenuItem1.setText("Altas");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+        jIemAsigar.setText("Ir");
+        jIemAsigar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem1ActionPerformed(evt);
+                jIemAsigarActionPerformed(evt);
             }
         });
-        jMenu1.add(jMenuItem1);
+        jMenu1.add(jIemAsigar);
 
         jMenuBar1.add(jMenu1);
 
-        jMenu2.setText("Inventario");
+        jItemReporte.setText("Reportes trabajo");
 
-        jMenuItem2.setText("Altas");
+        jMenuItem2.setText("Ir");
         jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem2ActionPerformed(evt);
             }
         });
-        jMenu2.add(jMenuItem2);
+        jItemReporte.add(jMenuItem2);
 
-        jMenuBar1.add(jMenu2);
+        jMenuBar1.add(jItemReporte);
 
         setJMenuBar(jMenuBar1);
 
@@ -91,18 +88,18 @@ public class Inicio extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-    //Método que permite ir a la ventana de altas para personal
-    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-       AltasPersonal altas = new AltasPersonal();
-       altas.setVisible(true);
-       dispose();
-    }//GEN-LAST:event_jMenuItem1ActionPerformed
-    //Método que permite ir a la ventana de altaas inventario
+
+    private void jIemAsigarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jIemAsigarActionPerformed
+        // TODO add your handling code here:
+        AsignarTrabajo asignarTrabajo = new AsignarTrabajo();
+        asignarTrabajo.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jIemAsigarActionPerformed
+
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        AltasInventario altas = new AltasInventario();
-        //Método que permite visualizar la ventana
-        altas.setVisible(true);
-        //Método que cierra la ventana para abrir otra
+        // TODO add your handling code here:
+        RecordsObtenidos obtenidos = new RecordsObtenidos();
+        obtenidos.setVisible(true);
         dispose();
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
@@ -145,11 +142,11 @@ public class Inicio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem jIemAsigar;
+    private javax.swing.JMenu jItemReporte;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     // End of variables declaration//GEN-END:variables
 }
