6a0ac945a8d6f1cbde6d7d80f4f6262fbcdcb870
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-17 16:03:29 -0600

    Uso de inseriones y modifiación de bitácoras

--- UnADM/basesdedatos/DABD_U3_A2_RARP/tiggers.sql
+++ UnADM/basesdedatos/DABD_U3_A2_RARP/tiggers.sql
@@ -8,3 +8,15 @@ VALUES (NOW(), 'Bienvenida', 'Bienvenida','06:00:00','Bienvenida','85','1000','2
 
 /* Verificar estado bitácora */
 SHOW VARIABLES like '%eneral_log%';
+
+/* Activar la bitácora general */
+SET GLOBAL general_log=1;
+
+/* Insertando datos */
+INSERT INTO usuario (nombre, apellido1, apellido2, fechanacimiento, correo, sexo) VALUES ('Carlos', 'León', 'Martinez', '1985-07-24', 'clm@gmail.com', 'Masculino');
+INSERT INTO usuario (nombre, apellido1, apellido2, fechanacimiento, correo, sexo) VALUES ('Isamar', 'Baldera', 'Pérez', '1992-09-02', 'ibp@gmail.com', 'Femenino');
+
+SELECT * FROM usuario;
+
+/* Describir la bitácora */
+DESCRIBE mysql.general_log;
