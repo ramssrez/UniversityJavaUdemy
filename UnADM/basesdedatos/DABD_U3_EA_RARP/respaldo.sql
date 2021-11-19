65cdcfa7c6a107c2bc5b844e8ce1fe6f76a43661
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-19 15:58:55 -0600

    Creación del respaldo de la base de datos lomitos

--- UnADM/basesdedatos/DABD_U3_EA_RARP/respaldo.sql
+++ UnADM/basesdedatos/DABD_U3_EA_RARP/respaldo.sql
@@ -5,3 +5,6 @@ C:\Users\ramss\Documents\respaldos
 
  /* Ejercicio de prueba */
 mysqldump -u root -p es1921023412 >C:\Users\ramss\Documents\respaldos\respaldo_es192.sql
+
+/* Respaldo de la base de datos de limitos */
+mysqldump -u root -p lomitos >C:\Users\ramss\Documents\respaldos\respaldo_lomitos.sql
