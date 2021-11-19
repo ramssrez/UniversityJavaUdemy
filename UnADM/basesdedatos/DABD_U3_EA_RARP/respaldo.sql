f8522859ba333de5dabace674fe7762169532120
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-19 15:56:04 -0600

    Creación del respaldo de ejemplo

---
+++ UnADM/basesdedatos/DABD_U3_EA_RARP/respaldo.sql
@@ -0,0 +1,7 @@
+/* 
+Direccion respaldo
+C:\Users\ramss\Documents\respaldos
+ */
+
+ /* Ejercicio de prueba */
+mysqldump -u root -p es1921023412 >C:\Users\ramss\Documents\respaldos\respaldo_es192.sql
