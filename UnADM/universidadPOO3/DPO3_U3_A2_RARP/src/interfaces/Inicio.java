b932084ff2acba6de29b6292a0fe5adb8f4db3ee
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-23 12:31:34 -0600

    Creación de la interfaz como aplicación del evento en el boton

--- UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/interfaces/Inicio.java
+++ UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/interfaces/Inicio.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package interfaces;
 
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -23,7 +19,7 @@ public class Inicio extends javax.swing.JFrame {
         this.setResizable(false);
     }
 
-        //Método que verifica que los campos no se encuentren vacios
+    //Método que verifica que los campos no se encuentren vacios
     public boolean validacionCamposTexto() {
         if ((jtfName.getText().equals("") && jtfSocialNumber.getText().equals("") && jtaSintomas.getText().equals(""))
                 || jtfName.getText().equals("") || jtfSocialNumber.getText().equals("") || jtaSintomas.getText().equals("")) {
@@ -34,6 +30,7 @@ public class Inicio extends javax.swing.JFrame {
             return true;
         }
     }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -69,39 +66,36 @@ public class Inicio extends javax.swing.JFrame {
 
         jbtnEnviar.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jbtnEnviar.setText("Enviar");
+        jbtnEnviar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnEnviarActionPerformed(evt);
+            }
+        });
 
         jtfName.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jtfName.setText("Jorge Negrete Sanchez");
         jtfName.setToolTipText("");
 
         jtfSocialNumber.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jtfSocialNumber.setText("589474036");
         jtfSocialNumber.setToolTipText("");
 
         jtaSintomas.setColumns(20);
         jtaSintomas.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jtaSintomas.setRows(5);
+        jtaSintomas.setText("Dolor abdominal, temperatura de 39 °C, se queja de dolor en todo el cuerpo");
+        jtaSintomas.setToolTipText("");
+        jtaSintomas.setAutoscrolls(false);
         jScrollPane1.setViewportView(jtaSintomas);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel3)
-                .addGap(99, 99, 99))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(145, 145, 145)
-                        .addComponent(jLabel4))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(122, 122, 122)
-                        .addComponent(jbtnEnviar)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(0, 0, Short.MAX_VALUE)
@@ -111,11 +105,25 @@ public class Inicio extends javax.swing.JFrame {
                                         .addGap(106, 106, 106)
                                         .addComponent(jLabel2))))
                             .addComponent(jtfName)
-                            .addComponent(jtfSocialNumber, javax.swing.GroupLayout.Alignment.TRAILING))
-                        .addContainerGap())
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jScrollPane1)
-                        .addContainerGap())))
+                            .addComponent(jtfSocialNumber, javax.swing.GroupLayout.Alignment.TRAILING)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(145, 145, 145)
+                                .addComponent(jLabel4))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(139, 139, 139)
+                                .addComponent(jbtnEnviar)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel3)
+                .addGap(99, 99, 99))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -132,15 +140,32 @@ public class Inicio extends javax.swing.JFrame {
                 .addComponent(jtfSocialNumber, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(35, 35, 35)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(222, 222, 222)
                 .addComponent(jbtnEnviar)
-                .addContainerGap(160, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jbtnEnviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnEnviarActionPerformed
+        // TODO add your handling code here:
+        if (validacionCamposTexto()) {
+            JOptionPane.showMessageDialog(null, "Los campos se encuentran llenos");
+            String nombre = jtfName.getText();
+            System.out.println("nombre = " + nombre);
+            String numero = jtfSocialNumber.getText();
+            System.out.println("numero = " + numero);
+            String sintomas = jtaSintomas.getText();
+            System.out.println("sintomas = " + sintomas);
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Los campos se encuentran vacios");
+
+        }
+    }//GEN-LAST:event_jbtnEnviarActionPerformed
 
     /**
      * @param args the command line arguments
