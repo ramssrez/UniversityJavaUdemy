9f3bfb44930c6cc30d88b9b747200a5dbdf9fd8d
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-22 21:27:08 -0600

    Aplicación de formato al servidor

--- UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/practica/Servidor.java
+++ UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/practica/Servidor.java
@@ -20,40 +20,41 @@ public class Servidor {
     public static void main(String[] args) {
         // TODO code application logic here
         //Declaración de variables para su utilización
-        ServerSocket serverSocket  = null;
+        ServerSocket serverSocket = null;
         Socket socket = null;
         DataInputStream in;
         DataOutputStream out;
         final int PUERTO = 5000;
-        
+
         //Uso del try/catch para el serverSocket
         try {
             //Inicialización del server con el puerto en donde va a estar funcionando, para este caso el puedto 5000
-            serverSocket  = new ServerSocket(PUERTO);
+            serverSocket = new ServerSocket(PUERTO);
             System.out.println("Servidor iniciado");
-            
-            while (true) {                
+
+            while (true) {
                 //Método en donde el servidor esta a la espera o escucha de las peticiones del cliente
                 System.out.println("Cliente conectado");
                 socket = serverSocket.accept();
-                
+
                 //Implementación de loos puentes para conectar con el servidor
                 in = new DataInputStream(socket.getInputStream());
                 out = new DataOutputStream(socket.getOutputStream());
-                
+
                 //Se queda a la espera a que el cliente escriba algo
                 String mensaje = in.readUTF();
-                
+
                 System.out.println(mensaje);
                 out.writeUTF("Hola mundo desde el servidor");
-                
+
                 socket.close();
                 System.out.println("Cliente desconectado");
-                
+
             }
         } catch (IOException ex) {
-            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error Selección: " + ex.getMessage());
+            ex.printStackTrace(System.out);
         }
     }
-    
+
 }
