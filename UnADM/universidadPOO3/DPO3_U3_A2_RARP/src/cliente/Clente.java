171fb04a6ecc77cbf7e1e19d1b03971e5065499d
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-22 21:28:22 -0600

    implementación del socket en la clase del cliente

--- UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/cliente/Clente.java
+++ UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/cliente/Clente.java
@@ -1,10 +1,26 @@
-
 package cliente;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author ramssrez
  */
 public class Clente {
-    
+
+    public static void main(String[] args) {
+        final String HOST = "127.0.0.1";
+        final int PUERTO = 5000;
+
+        try {
+            Socket sc = new Socket(HOST, PUERTO);
+        } catch (IOException ex) {
+            System.out.println("Error Selección: " + ex.getMessage());
+            ex.printStackTrace(System.out);;
+        }
+    }
+
 }
