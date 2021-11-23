5e33c638c1f87792baf3d663d255654d7252e033
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-23 12:40:41 -0600

    Creación del modelo del paciente

--- UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/objetos/PacienteAlta.java
+++ UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/objetos/PacienteAlta.java
@@ -6,5 +6,74 @@ package objetos;
  * @author ramssrez
  */
 public class PacienteAlta {
+    private String nombre;
+    private String numeroSocial;
+    private String sintomas;
+    private String doctorAsignado;
+    private String numeroConsultorio;
+    private String numeroTurno;
+
+    public PacienteAlta() {
+    }
+
+    public PacienteAlta(String nombre, String numeroSocial, String sintomas) {
+        this.nombre = nombre;
+        this.numeroSocial = numeroSocial;
+        this.sintomas = sintomas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumeroSocial() {
+        return numeroSocial;
+    }
+
+    public void setNumeroSocial(String numeroSocial) {
+        this.numeroSocial = numeroSocial;
+    }
+
+    public String getSintomas() {
+        return sintomas;
+    }
+
+    public void setSintomas(String sintomas) {
+        this.sintomas = sintomas;
+    }
+
+    public String getDoctorAsignado() {
+        return doctorAsignado;
+    }
+
+    public void setDoctorAsignado(String doctorAsignado) {
+        this.doctorAsignado = doctorAsignado;
+    }
+
+    public String getNumeroConsultorio() {
+        return numeroConsultorio;
+    }
+
+    public void setNumeroConsultorio(String numeroConsultorio) {
+        this.numeroConsultorio = numeroConsultorio;
+    }
+
+    public String getNumeroTurno() {
+        return numeroTurno;
+    }
+
+    public void setNumeroTurno(String numeroTurno) {
+        this.numeroTurno = numeroTurno;
+    }
+
+    @Override
+    public String toString() {
+        return "PacienteAlta{" + "nombre=" + nombre + ", numeroSocial=" + numeroSocial + ", sintomas=" + sintomas + ", doctorAsignado=" + doctorAsignado + ", numeroConsultorio=" + numeroConsultorio + ", numeroTurno=" + numeroTurno + '}';
+    }
+    
     
 }
