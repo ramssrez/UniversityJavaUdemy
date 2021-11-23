a782d6a289b35e6fb07bcec29c51c5cd8a27018c
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-22 21:14:03 -0600

    Creación del try/catxh para hacer uso del serversocket

--- UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/practica/Servidor.java
+++ UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/practica/Servidor.java
@@ -1,10 +1,12 @@
 package practica;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,6 +19,25 @@ public class Servidor {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Declaración de variables para su utilización
+        ServerSocket serverSocket  = null;
+        Socket socket = null;
+        DataInputStream in;
+        DataOutputStream out;
+        final int PUERTO = 5000;
+        //Uso del try/catch para el serverSocket
+        try {
+            //Inicialización del server con el puerto en donde va a estar funcionando, para este caso el puedto 5000
+            serverSocket  = new ServerSocket(PUERTO);
+            System.out.println("Servidor iniciado");
+            
+            while (true) {                
+                //Método en donde el servidor esta a la espera o escucha de las peticiones del cliente
+                socket = serverSocket.accept();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
