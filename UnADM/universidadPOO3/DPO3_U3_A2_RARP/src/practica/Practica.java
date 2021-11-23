0cdbe67514bcc15bd7b2124baeb57cc44caaab2f
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-22 21:04:24 -0600

    Creación los paquetes para los diferentes opcines de arquitectura

--- UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/practica/Practica.java
+++ UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/practica/Practica.java
@@ -1,3 +1,5 @@
+package practica;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,7 +10,7 @@
  *
  * @author ramssrez
  */
-public class main {
+public class Practica {
 
     /**
      * @param args the command line arguments
