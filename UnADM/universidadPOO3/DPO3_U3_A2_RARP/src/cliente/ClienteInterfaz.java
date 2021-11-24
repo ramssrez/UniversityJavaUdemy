f3ca7137aab16b5f572fedd66985ebded9ea321f
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-23 20:51:51 -0600

    Documentación de la clase

--- UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/cliente/ClienteInterfaz.java
+++ UnADM/universidadPOO3/DPO3_U3_A2_RARP/src/cliente/ClienteInterfaz.java
@@ -14,8 +14,9 @@ import objetos.PacienteAlta;
  * @author ramssrez
  */
 public class ClienteInterfaz extends javax.swing.JFrame implements Runnable {
-
+    //Declaración del host para correr el servidor y cliente en la misma maquina
     private final String HOST = "127.0.0.1";
+    //Declaración de los puertos tanto el servidor como el cliente
     private final int PUERTOSERVIDOR = 5000;
     private final int PUERTOCLIENTE = 5050;
 
@@ -28,8 +29,10 @@ public class ClienteInterfaz extends javax.swing.JFrame implements Runnable {
         this.setLocationRelativeTo(null);
         //Sentencia que desactiva el minimizar y maximizar de las ventanas principales
         this.setResizable(false);
+        //Creación del hilo para su implementación y se mantenga a la escucha del servidor
         Thread thread = new Thread(this);
         thread.start();
+        jtaDatosServidor.setEnabled(false);
     }
 
     //Método que verifica que los campos no se encuentren vacios
@@ -51,56 +54,69 @@ public class ClienteInterfaz extends javax.swing.JFrame implements Runnable {
         jtaSintomas.setText("");
     }
 
+    //Método que genera la conexión con el servidor 
     public void generarConexionServidor() {
+        //Obtención de los datos ingresados en las áreas de texto
         String nombre = jtfName.getText();
-        //System.out.println("nombre = " + nombre);
         String numero = jtfSocialNumber.getText();
-        //System.out.println("numero = " + numero);
         String sintomas = jtaSintomas.getText();
-        //System.out.println("sintomas = " + sintomas);
+        //Instancia del objeto paciente con lo que se obtuvo del formulacio
         PacienteAlta paciente = new PacienteAlta(nombre, numero, sintomas);
-        //System.out.println(paciente.toString());
-
+        //Try/Catch para el socket
         try {
+            //Instancia del socket con el uso del Host y el puerto del servidor
             Socket socket = new Socket(HOST, PUERTOSERVIDOR);
+            //Instancia del Stream para enviar el objeto por medio de la red
             ObjectOutputStream envioDatos = new ObjectOutputStream(socket.getOutputStream());
+            //Envio del objeto
             envioDatos.writeObject(paciente);
+            //Cierre de la coneción del servidor
             socket.close();
 
+            //Catch que registra algún error por parte del servidor
         } catch (IOException ex) {
-            System.out.println("Error Selección: " + ex.getMessage());
+            System.out.println("Error IOException: " + ex.getMessage());
             ex.printStackTrace(System.out);;
         }
 
     }
 
-    //]Uso del método run, el cual es necesario usar por la implementación de runneable
+    //]Uso del método run, el cual es necesario usar por la implementación de runneable y se mantenga a la escucha del servidor
     @Override
     public void run() {
         System.out.println("Esto es desde el hilo cliente");
+        //DElcaración de la variable del paciente
         PacienteAlta pacienteAsignado;
         
+        //Try/catch para hacer el Cliente como un servidor, con su propio puerto
         try {
+            //Declaración serverSocket asignando un puerto exclusivo para el cliente
             ServerSocket servidor = new ServerSocket(PUERTOCLIENTE);
+            //Escucha del Cleitne para cuando el servidor mande los objetos creados
             while (true) {
+                //Declaración del socket para inicializar un servidor dentro de Cliente
                 Socket socket = servidor.accept();
+                //DEclaración del Stream para obtener la información o datos que envie el servidor
                 ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+                //Parseo del objeto que venga del servidor a un PacienteAlta
                 pacienteAsignado = (PacienteAlta) inputStream.readObject();
-                System.out.println(pacienteAsignado.toString());
+                //Impresión del mensaje de todos los datos que se obtuvieron del servidor
                 String mensaje = "Turno: " + pacienteAsignado.getNumeroTurno() + 
                         "\nNúmero Seguro Social: " + pacienteAsignado.getNumeroSocial() +
                         "\nNombre: " + pacienteAsignado.getNombre() +
                         "\nSintomas: " + pacienteAsignado.getSintomas() +
                         "\nNombre Doctor: " + pacienteAsignado.getDoctorAsignado() +
                         "\nNúmero consultorio: " + pacienteAsignado.getNumeroConsultorio();
+                //Impresión del mensaje en el área de texto
                 jtaDatosServidor.setText(mensaje);
             }
-
+            //Excepción para el el caso del ServerSocket
         } catch (IOException ex) {
-            System.out.println("Error Selección: " + ex.getMessage());
+            System.out.println("Error IOException: " + ex.getMessage());
             ex.printStackTrace(System.out);
+            //Excepción para el caso de que no se encuentre un objeto desde el servidor
         } catch (ClassNotFoundException ex) {
-            System.out.println("Error Selección: " + ex.getMessage());
+            System.out.println("Error ClassNotFound: " + ex.getMessage());
             ex.printStackTrace(System.out);
         }
     }
@@ -125,6 +141,7 @@ public class ClienteInterfaz extends javax.swing.JFrame implements Runnable {
         jtaSintomas = new javax.swing.JTextArea();
         jScrollPane2 = new javax.swing.JScrollPane();
         jtaDatosServidor = new javax.swing.JTextArea();
+        jbtnLimpiar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -171,6 +188,16 @@ public class ClienteInterfaz extends javax.swing.JFrame implements Runnable {
         jtaDatosServidor.setAutoscrolls(false);
         jScrollPane2.setViewportView(jtaDatosServidor);
 
+        jbtnLimpiar.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jbtnLimpiar.setText("Limpiar Campos");
+        jbtnLimpiar.setToolTipText("");
+        jbtnLimpiar.setActionCommand("Limpiar Campos");
+        jbtnLimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnLimpiarActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -199,17 +226,23 @@ public class ClienteInterfaz extends javax.swing.JFrame implements Runnable {
                 .addComponent(jLabel3)
                 .addGap(99, 99, 99))
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1)
-                .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addGap(127, 127, 127)
-                .addComponent(jbtnEnviar)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(127, 127, 127)
+                        .addComponent(jbtnEnviar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(123, 123, 123)
+                        .addComponent(jbtnLimpiar)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane2)
-                .addContainerGap())
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jScrollPane2)
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -230,24 +263,34 @@ public class ClienteInterfaz extends javax.swing.JFrame implements Runnable {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jbtnEnviar)
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jbtnLimpiar)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
+    //Evento del botón para el envio del objeto
     private void jbtnEnviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnEnviarActionPerformed
-        jtaDatosServidor.setText("");
+        //Validación de los campos para que no se encuentren vacios
         if (validacionCamposTexto()) {
+            //Llamado al método para la conexión del servidor
             generarConexionServidor();
         } else {
+            //Dialog para el caso de que los campos se encuentran vacios
             JOptionPane.showMessageDialog(null, "Los campos se encuentran vacios");
-
         }
+        //Llamado el método para limpiat los campos 
         limpiarCampos();
     }//GEN-LAST:event_jbtnEnviarActionPerformed
+
+    private void jbtnLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnLimpiarActionPerformed
+        // TODO add your handling code here:
+        limpiarCampos();
+        jtaDatosServidor.setText("");
+    }//GEN-LAST:event_jbtnLimpiarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -294,6 +337,7 @@ public class ClienteInterfaz extends javax.swing.JFrame implements Runnable {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JButton jbtnEnviar;
+    private javax.swing.JButton jbtnLimpiar;
     private javax.swing.JTextArea jtaDatosServidor;
     private javax.swing.JTextArea jtaSintomas;
     private javax.swing.JTextField jtfName;
