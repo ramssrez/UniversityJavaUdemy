6d37382f07960766062e519256d824050a5b41b5
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-24 13:07:37 -0600

    Desarrollo de la interfaz del Cliente

--- UnADM/universidadPOO3/La Hamburguesa/src/cliente/Cliente.java
+++ UnADM/universidadPOO3/La Hamburguesa/src/cliente/Cliente.java
@@ -9,7 +9,7 @@ package cliente;
  *
  * @author ramssrez
  */
-public class Cliente extends javax.swing.JFrame implements Runnable{
+public class Cliente extends javax.swing.JFrame implements Runnable {
 
     //Declaración del host para correr el servidor y cliente en la misma maquina
     private final String HOST = "127.0.0.1";
@@ -29,8 +29,13 @@ public class Cliente extends javax.swing.JFrame implements Runnable{
         //Creación del hilo para su implementación y se mantenga a la escucha del servidor
         Thread thread = new Thread(this);
         thread.start();
+        btngRadios.add(jrbPackOne);
+        btngRadios.add(jrbPackTwo);
+        btngRadios.add(jrbPackThree);
+        btngRadios.add(jrbPackFor);
+        jtStatus.setEditable(false);
     }
-    
+
     @Override
     public void run() {
         System.out.println("Esto es un hilo desde el Cliente");
@@ -45,17 +50,176 @@ public class Cliente extends javax.swing.JFrame implements Runnable{
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btngRadios = new javax.swing.ButtonGroup();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jtfName = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jtfPhone = new javax.swing.JTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jtAdress = new javax.swing.JTextArea();
+        jrbPackOne = new javax.swing.JRadioButton();
+        jrbPackTwo = new javax.swing.JRadioButton();
+        jrbPackThree = new javax.swing.JRadioButton();
+        jrbPackFor = new javax.swing.JRadioButton();
+        jLabel6 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jLabel7 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jtStatus = new javax.swing.JTextArea();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(0, 204, 255));
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel1.setText("La Hamburguesa");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel2.setText("Nombre");
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel3.setText("Ingresa los siguientes datos");
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel4.setText("Teléfono");
+
+        jtfName.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jtfName.setText("Jorge Negrete Sanchez");
+        jtfName.setToolTipText("");
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel5.setText("Dirección de entrega");
+
+        jtfPhone.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jtfPhone.setText("5555555552");
+        jtfPhone.setToolTipText("");
+
+        jtAdress.setColumns(20);
+        jtAdress.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jtAdress.setRows(5);
+        jtAdress.setText("Av. Revolución #9, Colonia San Pedro Culhuacan\nTexcoco, Estado de México\n");
+        jtAdress.setToolTipText("");
+        jtAdress.setAutoscrolls(false);
+        jScrollPane2.setViewportView(jtAdress);
+
+        jrbPackOne.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jrbPackOne.setText("Pack 1");
+
+        jrbPackTwo.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jrbPackTwo.setText("Pack 2");
+
+        jrbPackThree.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jrbPackThree.setText("Pack 3");
+
+        jrbPackFor.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jrbPackFor.setText("Pack 4");
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel6.setText("Escoge alguno de nuestro paquete");
+
+        jButton1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jButton1.setText("Realizar Pedido");
+
+        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel7.setText("Status del Pedido");
+
+        jtStatus.setColumns(20);
+        jtStatus.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jtStatus.setRows(5);
+        jtStatus.setToolTipText("");
+        jtStatus.setAutoscrolls(false);
+        jScrollPane3.setViewportView(jtStatus);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(94, 94, 94))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel6)
+                        .addGap(86, 86, 86))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(129, 129, 129)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel7)
+                    .addComponent(jButton1))
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(169, 169, 169)
+                                .addComponent(jLabel2))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(164, 164, 164)
+                                .addComponent(jLabel4))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(124, 124, 124)
+                                .addComponent(jLabel5))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(69, 69, 69)
+                                .addComponent(jLabel3))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(156, 156, 156)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jrbPackTwo)
+                                    .addComponent(jrbPackOne)
+                                    .addComponent(jrbPackThree)
+                                    .addComponent(jrbPackFor))))
+                        .addGap(0, 59, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jtfName)
+                            .addComponent(jtfPhone, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jScrollPane3))))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(1, 1, 1)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jtfName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel4)
+                .addGap(18, 18, 18)
+                .addComponent(jtfPhone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel6)
+                .addGap(9, 9, 9)
+                .addComponent(jrbPackOne)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jrbPackTwo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jrbPackThree)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jrbPackFor)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel7)
+                .addGap(1, 1, 1)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(33, Short.MAX_VALUE))
         );
 
         pack();
@@ -98,5 +262,24 @@ public class Cliente extends javax.swing.JFrame implements Runnable{
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup btngRadios;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JRadioButton jrbPackFor;
+    private javax.swing.JRadioButton jrbPackOne;
+    private javax.swing.JRadioButton jrbPackThree;
+    private javax.swing.JRadioButton jrbPackTwo;
+    private javax.swing.JTextArea jtAdress;
+    private javax.swing.JTextArea jtStatus;
+    private javax.swing.JTextField jtfName;
+    private javax.swing.JTextField jtfPhone;
     // End of variables declaration//GEN-END:variables
 }
