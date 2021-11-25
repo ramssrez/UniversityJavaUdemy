f143ef772608f142c51adcb8b816394164455678
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-25 11:30:47 -0600

    Creacion del modelo de personal

---
+++ UnADM/universidadPOO3/DPO3_U3_AC_RARP/Estancia/src/objetos/Personall.java
@@ -0,0 +1,97 @@
+package objetos;
+
+/**
+ *
+ * @author ramssrez
+ */
+public class Personall {
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private String area;
+    private String edad;
+    private String numeroEmpleado;
+    private String puesto;
+    private String sueldo;
+
+    public Personall() {
+    }
+
+    public Personall(String nombre, String apellidos, String area, String edad, String numeroEmpleado, String puesto, String sueldo) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.area = area;
+        this.edad = edad;
+        this.numeroEmpleado = numeroEmpleado;
+        this.puesto = puesto;
+        this.sueldo = sueldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getNumeroEmpleado() {
+        return numeroEmpleado;
+    }
+
+    public void setNumeroEmpleado(String numeroEmpleado) {
+        this.numeroEmpleado = numeroEmpleado;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public String getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(String sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Personall{" + "id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", area=" + area + ", edad=" + edad + ", numeroEmpleado=" + numeroEmpleado + ", puesto=" + puesto + ", sueldo=" + sueldo + '}';
+    }
+    
+    
+
+}
