48d02396d1cb39ed7587752bdebe975e6b4e15d6
Raul Ramirez Perez <ramssrez@gmail.com>
2021-11-25 11:40:55 -0600

    Creación de la clase personalDAO

---
+++ UnADM/universidadPOO3/DPO3_U3_AC_RARP/Estancia/src/database/PersonalDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+/**
+ *
+ * @author ramssrez
+ */
+public class PersonalDAO {
+     //Declaración de la sentencia a realizar para insertar un registro a  la base de datos
+    private static final String SQL_INSERT = "INSERT INTO personla(nombre, apellidos, numero_empleado, edad, puesto, sueldo, area) VALUES(?,?,?,?,?,?,?)";
+
+}
