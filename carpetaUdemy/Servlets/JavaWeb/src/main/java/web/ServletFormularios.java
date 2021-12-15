390fb58c34d2148411b9d84b6b29d5ed45e8ab3f
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-15 11:44:20 -0600

    Creación y modificación de los recursos

--- carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletFormularios.java
+++ carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletFormularios.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package web;
 
+import javax.servlet.http.HttpServlet;
 /**
  *
  * @author ramssrez
  */
-public class ServletFormularios {
+public class ServletFormularios extends HttpServlet{
     
 }
