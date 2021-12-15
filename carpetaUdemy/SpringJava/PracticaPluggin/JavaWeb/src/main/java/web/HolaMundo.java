ee498d3ddabc05573ebf8829fbe6e28c010e7b1a
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-15 09:05:44 -0600

    Creación del metodo que retorna información

--- carpetaUdemy/SpringJava/PracticaPluggin/JavaWeb/src/main/java/web/HolaMundo.java
+++ carpetaUdemy/SpringJava/PracticaPluggin/JavaWeb/src/main/java/web/HolaMundo.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package web;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 /**
  *
  * @author ramssrez
  */
-public class HolaMundo {
+//Anotación de la ruta de nuetra pagina
+@WebServlet("/HolaMundo")
+public class HolaMundo extends HttpServlet{
+
+    //Uso del método doGet para poder reazliar la petición y el resultado
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+        //Glassfishe realiza la petición
+    }
     
 }
