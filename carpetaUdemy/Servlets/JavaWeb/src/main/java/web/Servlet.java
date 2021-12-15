c78fc6a8cd581418401486ea926b24845abb1eeb
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-15 10:16:50 -0600

    Creación y uso del metodo para poder obtener los datos que vengan de un formulario

--- carpetaUdemy/Servlets/JavaWeb/src/main/java/web/Servlet.java
+++ carpetaUdemy/Servlets/JavaWeb/src/main/java/web/Servlet.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author ramssrez
  */
-public class Servlet {
-    
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse respond) throws ServletException, IOException {
+        //Leer los parametros del formulario HTML
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+
+        System.out.println("usuario = " + usuario);
+        System.out.println("password = " + password);
+
+        PrintWriter out = respond.getWriter();
+        out.print("<html>");
+        out.print("<body>");
+        out.print("El parametro usuario es: " + usuario);
+        out.print("<br/>");
+        out.print("El parametro password es: " + password);
+        out.print("</body>");
+        out.print("</html>");
+    }
+
 }
