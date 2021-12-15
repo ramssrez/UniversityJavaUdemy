a9e4cf27b04f867b1def1551f3fdeddc86de3aea
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-15 09:11:13 -0600

    Creación del archivo para el primer hola mundo desde una clases en java

--- carpetaUdemy/SpringJava/PracticaPluggin/JavaWeb/src/main/java/web/HolaMundo.java
+++ carpetaUdemy/SpringJava/PracticaPluggin/JavaWeb/src/main/java/web/HolaMundo.java
@@ -1,5 +1,7 @@
 package web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,8 +16,11 @@ public class HolaMundo extends HttpServlet{
 
     //Uso del método doGet para poder reazliar la petición y el resultado
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
         //Glassfishe realiza la petición
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("Hola mundo desde Servlets");
     }
     
 }
