61cb15b891725df0d10f7a32a6020d73a5c5f440
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-17 08:57:27 -0600

    Impresión del metodo http usado

--- carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletHeader.java
+++ carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletHeader.java
@@ -1,11 +1,27 @@
 package web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author ramssrez
  */
+
+@WebServlet("/ServletHeaders")
 public class ServletHeader extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String metodoHttp = request.getMethod();
+        System.out.println("metodoHttp = " + metodoHttp);
+    }
     
 }
