6407b0cf67bcb3e7688631af76c3f432827dac46
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-17 04:34:53 -0600

    Validación de formularios despues

--- carpetaUdemy/Servlets/JavaWeb/src/main/webapp/recursos/funciones.js
+++ carpetaUdemy/Servlets/JavaWeb/src/main/webapp/recursos/funciones.js
@@ -1,20 +1,50 @@
-function validarForm(forma){
+function validarForm(forma) {
     var usuario = forma.usuario;
     var password = forma.password;
     var tecnologias = forma.tecnologia;
-    var checkSEleccionado = false;
- 
-    if(usuario.value === "" || usuario.value === "Escribir usuario"){
+    var checkSeleccionado = false;
+    var generos = forma.genero;
+    var radioSeleccionado = false;
+    var ocupacion = forma.ocupacion;
+
+    if (usuario.value === "" || usuario.value === "Escribir usuario") {
         alert("Debe proporcionar un nombre de usuario");
         usuario.focus();
         usuario.select();
         return false;
     }
-    
-    if(password.value === "" || password.value.length < 3){
+
+    if (password.value === "" || password.value.length < 3) {
         alert("Debe proporcionar una contraseña al menos de 3 caracteres");
         password.focus();
         password.select();
+        return false;
+    }
+
+    for (var i = 0; i < tecnologias.length; i++) {
+        if (tecnologias[i].checked) {
+            checkSeleccionado = true;
+        }
+    }
+
+    if (!checkSeleccionado) {
+        alert("Debe seleccionar una tecnología")
+        return false;
+    }
+
+    for (var i = 0; i < generos.length; i++) {
+        if (generos[i].checked) {
+            radioSeleccionado = true;
+        }
+    }
+    
+    if (!radioSeleccionado) {
+        alert("Dede seleccionar un genero");
+        return false;
+    }
+    
+    if(ocupacion.value === ""){
+        alert("Debe seleccionar una ocupación");
         return false;
     }
 
