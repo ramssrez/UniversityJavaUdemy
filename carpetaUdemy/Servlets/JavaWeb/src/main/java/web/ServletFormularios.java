dea87e1edb25a7ff03e7f6df6971dbd60b1b0b13
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-17 05:11:38 -0600

    Integración de las variables para observar lo que se envia desde el html

--- carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletFormularios.java
+++ carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletFormularios.java
@@ -1,12 +1,46 @@
 package web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  * @author ramssrez
  */
 @WebServlet("/ServletFormulario")
-public class ServletFormularios extends HttpServlet{
-    
+public class ServletFormularios extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String usuario = request.getParameter("usuario");
+        System.out.println("usuario = " + usuario);
+        
+        String password = request.getParameter("password");
+        System.out.println("password = " + password);
+        
+        String tecnologias[] = request.getParameterValues("tecnologia");
+        System.out.println("tecnologias = " + tecnologias);
+        
+        String genero = request.getParameter("genero");
+        System.out.println("genero = " + genero);
+        
+        String ocupacion = request.getParameter("ocupacion");
+        System.out.println("ocupacion = " + ocupacion);
+        
+        String musica[] = request.getParameterValues("musica");
+        System.out.println("musica = " + musica);
+        
+        String comentario = request.getParameter("comentarios");
+        System.out.println("comentario = " + comentario);
+        
+    }
+
 }
