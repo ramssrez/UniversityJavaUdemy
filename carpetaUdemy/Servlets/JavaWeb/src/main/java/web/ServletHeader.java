b92288c71a7b92890f5f3b8f6b4a60cc06c8fab0
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-17 09:21:23 -0600

    Identificación de cabeceros dentro de una petición http

--- carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletHeader.java
+++ carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletHeader.java
@@ -2,6 +2,7 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,16 +13,27 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author ramssrez
  */
-
 @WebServlet("/ServletHeaders")
-public class ServletHeader extends HttpServlet{
+public class ServletHeader extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
+
+        //Impresión del metodo http usado
         String metodoHttp = request.getMethod();
         System.out.println("metodoHttp = " + metodoHttp);
+
+        //Impresión de la uri utilizado
+        String uri = request.getRequestURI();
+        System.out.println("uri = " + uri);
+        
+        Enumeration cabeceros = request.getHeaderNames();
+        while(cabeceros.hasMoreElements()){
+            String nombreCabecero = (String) cabeceros.nextElement();
+            System.out.println("nombreCabecero = " + nombreCabecero + " : " + request.getHeader(nombreCabecero));
+        }
     }
-    
+
 }
