8f2b628909bafc1c706c4b58e96dff22fb17bfbf
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-17 04:21:57 -0600

    Integración del JavaScript para validaciones

--- carpetaUdemy/Servlets/JavaWeb/target/JavaWeb-1.0/recursos/funciones.js
+++ carpetaUdemy/Servlets/JavaWeb/target/JavaWeb-1.0/recursos/funciones.js
@@ -1,2 +1,22 @@
+function validarForm(forma){
+    var usuario = forma.usuario;
+    var password = forma.password;
+    var tecnologias = forma.tecnologia;
+    var checkSEleccionado = false;
+ 
+    if(usuario.value === "" || usuario.value === "Escribir usuario"){
+        alert("Debe proporcionar un nombre de usuario");
+        usuario.focus();
+        usuario.select();
+        return false;
+    }
+    
+    if(password.value === "" || password.value.length < 3){
+        alert("Debe proporcionar una contraseña al menos de 3 caracteres");
+        password.focus();
+        password.select();
+        return false;
+    }
 
+}
 
