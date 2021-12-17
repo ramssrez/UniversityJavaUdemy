8cac04f796a36bd0faa7a42dcdda773edca4cb14
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-17 07:00:43 -0600

    Impresión de los datos de usuario y contraseña en otra ventana

--- carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletFormularios.java
+++ carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletFormularios.java
@@ -22,25 +22,56 @@ public class ServletFormularios extends HttpServlet {
 
         String usuario = request.getParameter("usuario");
         System.out.println("usuario = " + usuario);
-        
+
         String password = request.getParameter("password");
         System.out.println("password = " + password);
-        
+
         String tecnologias[] = request.getParameterValues("tecnologia");
         System.out.println("tecnologias = " + tecnologias);
-        
+
         String genero = request.getParameter("genero");
         System.out.println("genero = " + genero);
-        
+
         String ocupacion = request.getParameter("ocupacion");
         System.out.println("ocupacion = " + ocupacion);
-        
+
         String musica[] = request.getParameterValues("musica");
         System.out.println("musica = " + musica);
-        
+
         String comentario = request.getParameter("comentarios");
         System.out.println("comentario = " + comentario);
-        
+
+        out.print("<html>");
+
+        out.print("<head>");
+        out.print("<title>Resultado Servlet</title>");
+        out.print("</head>");
+
+        out.print("<body>");
+        out.print("<h1>Parametros procesados por el Servlet</h1>");
+        out.print("<table border='1'>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Usuario");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(usuario);
+        out.print("</td>");
+        out.print("</tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Password");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(password);
+        out.print("</td>");
+        out.print("</tr>");
+
+        out.print("</table>");
+        out.print("</body>");
+        out.print("</html>");
     }
 
 }
