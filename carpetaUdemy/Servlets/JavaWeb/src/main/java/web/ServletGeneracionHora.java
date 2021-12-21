48f327795e73a897a3e4b8c1fe3d12d3b904d9a5
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-21 16:53:21 +0100

    Creación de los servlets para generar un archivo xml y la actualización del tiemp

--- carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletGeneracionHora.java
+++ carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletGeneracionHora.java
@@ -1,7 +1,14 @@
 package web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
@@ -10,6 +17,16 @@ import javax.servlet.http.HttpServlet;
 
 @WebServlet("/GeneracionHora")
 public class ServletGeneracionHora extends HttpServlet{
-    
-    
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.setHeader("refresh", "1");
+        Date fecha = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        String horaFormato = format.format(fecha);
+        PrintWriter out = response.getWriter();
+        out.print("Hora actualizada: " + horaFormato);
+        out.close();
+    }  
 }
