e138d3b7cbc2a60b4e46d6657c68f1846e1aff28
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-21 11:20:56 +0100

    Creación de clase para hacer uso de los codigos de estado

--- carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletEstados.java
+++ carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletEstados.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author ramssrez
  */
-public class ServletEstados {
-    
+@WebServlet("/ServletEstados")
+public class ServletEstados extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Simulación los valores correctos
+        String usuarioOk = "Juan";
+        String passwordOk = "123";
+
+        String usuaurio = req.getParameter("usuario");
+        String password = req.getParameter("password");
+
+        PrintWriter out = resp.getWriter();
+
+        if (usuarioOk.equals(usuaurio) && passwordOk.equals(password)) {
+            out.print("<h1>");
+            out.print("Datos correctos");
+            out.print("<br>Usuario: " + usuaurio);
+            out.print("<br>Password: " + password);
+            out.print("</h1>");
+        }else{
+            resp.sendError(resp.SC_UNAUTHORIZED,"Las credenciales son erroneas");
+        }
+        out.close();
+    }
+
 }
