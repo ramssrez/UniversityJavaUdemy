cd5af2da43818c1e5dfc67811913ab3dc7fe4a22
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-21 15:40:27 +0100

    Creación de los servlet

---
+++ carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletGeneracionExcel.java
@@ -0,0 +1,14 @@
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author ramssrez
+ */
+@WebServlet("GeneracionExcel")
+public class ServletGeneracionExcel extends HttpServlet{
+    
+    
+}
---
+++ carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletGeneracionHora.java
@@ -0,0 +1,15 @@
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author ramssrez
+ */
+
+@WebServlet("GeneracionHora")
+public class ServletGeneracionHora extends HttpServlet{
+    
+    
+}
