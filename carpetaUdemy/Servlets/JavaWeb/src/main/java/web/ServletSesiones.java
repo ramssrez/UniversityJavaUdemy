8897dffd14ee52336e8782af5fb41a705d1785db
Raul Ramirez Perez <ramssrez@gmail.com>
2021-12-22 14:55:29 +0100

    Finalización del servlet de sesiones

--- carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletSesiones.java
+++ carpetaUdemy/Servlets/JavaWeb/src/main/java/web/ServletSesiones.java
@@ -2,7 +2,6 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,35 +13,38 @@ import javax.servlet.http.HttpSession;
  * @author ramssrez
  */
 @WebServlet("/ServletSesiones")
-public class ServletSesiones extends HttpServlet{
+public class ServletSesiones extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession sesion = request.getSession();
         String titulo = null;
-        
+
         //Pedir el atributo contadorVisistas a la sesion
         Integer contadorVisitas = (Integer) sesion.getAttribute("contadorVisitas");
-        
+        System.out.println("contadorVisitas = " + contadorVisitas);
+
         //Si es nulo es la primera vez que se visita el sitio
-        if(contadorVisitas != null){
+        if (contadorVisitas == null) {
             contadorVisitas = 1;
             titulo = "Bienvenido por primera vez";
-        }else{
+        } else {
             contadorVisitas++;
             titulo = "Bienvenido nuevamente";
         }
-        
+
         //Agregamos el nuevo valor a la sesión
         sesion.setAttribute("contadorVisitas", contadorVisitas);
-        
+
         //Mandamos la respuesta al cliente
-        PrintWriter out= response.getWriter();
+        PrintWriter out = response.getWriter();
+        out.print("Titulo: " + titulo);
+        out.print("<br>");
         out.print("no. accesos al recurso: " + contadorVisitas);
         out.print("<br>");
         out.print("Id de la sesión: " + sesion.getId());
         out.close();
     }
-        
+
 }
