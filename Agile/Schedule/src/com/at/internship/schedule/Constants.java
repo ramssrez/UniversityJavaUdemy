c944eac58990e2651fcdd9e7342b59cc3d8ae5dd
Raul Ramirez Perez <ramssrez@gmail.com>
2022-03-24 18:17:45 +0100

    Creación delas clases de constantes

---
+++ Agile/Schedule/src/com/at/internship/schedule/Constants.java
@@ -0,0 +1,7 @@
+package com.at.internship.schedule;
+
+import java.io.File;
+
+public class Constants {
+    public static final String SERIALIZATION_PATH = "."+ File.separator;
+}
--- Agile/Schedule/src/com/at/internship/schedule/serialization/csv/ContactSerializer.java
+++ Agile/Schedule/src/com/at/internship/schedule/serialization/csv/ContactSerializer.java
@@ -1,8 +1,6 @@
 package com.at.internship.schedule.serialization.csv;
 
 public class ContactSerializer {
-    
-
-
+public static final String FILE_NAME = "Contact.csv";
 
 }
