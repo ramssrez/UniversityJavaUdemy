51042f93c2688f8bdb4d135e542a0ddffd5fbe71
Raul Ramirez Perez <ramssrez@gmail.com>
2022-03-29 13:44:21 +0200

    Ingreso de los metodos del circulo

--- Agile/Generics/src/Circulo.java
+++ Agile/Generics/src/Circulo.java
@@ -1,11 +1,22 @@
 public class Circulo extends Figura{
+    private double radio;
+
     @Override
     public void calcularPerimetro() {
-
+        super.perimetro = (this.radio * 2) * Math.PI;
     }
 
     @Override
     public void calcularArea() {
+        super.area = Math.pow(this.radio,2) * Math.PI;
+    }
 
+    @Override
+    public String toString() {
+        return "Circulo{" +
+                "radio=" + radio +
+                ", perimetro=" + perimetro +
+                ", area=" + area +
+                '}';
     }
 }
