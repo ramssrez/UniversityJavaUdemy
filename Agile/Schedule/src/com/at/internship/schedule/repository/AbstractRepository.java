f496f097b1ff40cf19205eb71b46c338a2ce037d
Raul Ramirez Perez <ramssrez@gmail.com>
2022-04-07 11:55:56 +0200

    Creación de los dos proyectos de maven uno apartir de la consola y otro apartir del ide

--- Agile/Schedule/src/com/at/internship/schedule/repository/AbstractRepository.java
+++ Agile/Schedule/src/com/at/internship/schedule/repository/AbstractRepository.java
@@ -2,13 +2,18 @@ package com.at.internship.schedule.repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class AbstractRepository <T, K>{
     private List<T> table;
+
     protected AbstractRepository(){
         table = new ArrayList<>();
     }
+
     public List<T> findAll(){
-        return table.stream().map()
+        return table.stream().map(this::clonesRow).collect(Collectors.toList());
     }
+
+    protected abstract T clonesRow(T source);
 }
