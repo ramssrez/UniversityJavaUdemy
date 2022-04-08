7f6919a9b53f42fc886964cf48148e3cc3a88964
Raul Ramirez Perez <ramssrez@gmail.com>
2022-04-08 19:39:15 +0200

    Inicio de las pruebas

--- Agile/junit-demo/src/test/java/com/at/internship/finance/InvestmentTest.java
+++ Agile/junit-demo/src/test/java/com/at/internship/finance/InvestmentTest.java
@@ -1,2 +1,19 @@
-package com.at.internship.finance;public class InvestmentTest {
+package com.at.internship.finance;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class InvestmentTest {
+    private Investment investment;
+
+    @Test
+    public void testGetFinalCapital(){
+        investment = new Investment(new BigDecimal("100.00"),new BigDecimal("0.10"));
+        //End of first year = 110;
+        //End of second year = 121;
+        //End of third year = 133.1
+        Assertions.assertEquals(new BigDecimal("133.10"),investment.getFinalCapital(3));
+    }
 }
