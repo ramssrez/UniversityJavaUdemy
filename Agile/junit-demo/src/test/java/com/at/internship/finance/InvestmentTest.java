d5d01a6c37fa874c9e0d168f46520929422ce200
Raul Ramirez Perez <ramssrez@gmail.com>
2022-04-08 19:45:46 +0200

    Creación del test para la inversión obtenida

--- Agile/junit-demo/src/test/java/com/at/internship/finance/InvestmentTest.java
+++ Agile/junit-demo/src/test/java/com/at/internship/finance/InvestmentTest.java
@@ -4,16 +4,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class InvestmentTest {
     private Investment investment;
 
     @Test
-    public void testGetFinalCapital(){
-        investment = new Investment(new BigDecimal("100.00"),new BigDecimal("0.10"));
+    public void testGetFinalCapital() {
+        investment = new Investment(new BigDecimal("100.00"), new BigDecimal("0.10"));
         //End of first year = 110;
         //End of second year = 121;
         //End of third year = 133.1
-        Assertions.assertEquals(new BigDecimal("133.10"),investment.getFinalCapital(3));
+        Assertions.assertEquals(new BigDecimal("133.10"), investment.getFinalCapital(3).setScale(2,RoundingMode.HALF_UP));
+    }
+
+    @Test
+    public void testGetYield() {
+        investment = new Investment(new BigDecimal("100.00"), new BigDecimal("0.10"));
+        //End of first year = 110;
+        //End of second year = 121;
+        //End of third year = 133.1
+        Assertions.assertEquals(new BigDecimal("33.10"), investment.getYield(3).setScale(2, RoundingMode.HALF_UP));
     }
 }
