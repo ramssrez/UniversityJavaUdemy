2db388dee1adee57f2bfe92507883817599e4319
Raul Ramirez Perez <ramssrez@gmail.com>
2022-04-08 19:56:26 +0200

    Creación de test para validar excepciónes

--- Agile/junit-demo/src/test/java/com/at/internship/finance/InvestmentTest.java
+++ Agile/junit-demo/src/test/java/com/at/internship/finance/InvestmentTest.java
@@ -1,7 +1,6 @@
 package com.at.internship.finance;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -11,7 +10,6 @@ public class InvestmentTest {
 
     @Test
     public void testGetFinalCapital() {
-        investment = new Investment(new BigDecimal("100.00"), new BigDecimal("0.10"));
         //End of first year = 110;
         //End of second year = 121;
         //End of third year = 133.1
@@ -20,10 +18,33 @@ public class InvestmentTest {
 
     @Test
     public void testGetYield() {
-        investment = new Investment(new BigDecimal("100.00"), new BigDecimal("0.10"));
         //End of first year = 110;
         //End of second year = 121;
         //End of third year = 133.1
         Assertions.assertEquals(new BigDecimal("33.10"), investment.getYield(3).setScale(2, RoundingMode.HALF_UP));
+    }
+    @BeforeEach
+    public void beforeEach(){
+        System.out.println("Before each test");
+        investment = new Investment(new BigDecimal("100.00"), new BigDecimal("0.10"));
+    }
+
+    @AfterAll
+    static void afterEach(){
+        System.out.println("After all test");
+    }
+
+    @BeforeEach
+    public void beforeAll(){
+        System.out.println("Before all test");
+    }
+
+    @Test
+    public void testValidatePeriodCountProvideNegative(){
+        IllegalArgumentException e = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                ()-> investment.validatePeriodCount(0)
+        );
+        Assertions.assertEquals("The number of periods should be 1 at minimum",e.getMessage());
     }
 }
