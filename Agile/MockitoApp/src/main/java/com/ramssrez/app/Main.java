902f0eaf8bd57983382578ec5db134bbca36240a
Raul Ramirez Perez <ramssrez@gmail.com>
2022-04-11 20:13:40 +0200

    Creación de la forma de inyeccion de dependenciaas

--- Agile/MockitoApp/src/main/java/com/ramssrez/app/Main.java
+++ Agile/MockitoApp/src/main/java/com/ramssrez/app/Main.java
@@ -1,12 +1,15 @@
 package com.ramssrez.app;
 
 import com.ramssrez.app.models.Alumno;
+import com.ramssrez.app.repositorio.AlumnoRepositoryImp;
+import com.ramssrez.app.repositorio.IAlumnoRepository;
 import com.ramssrez.app.services.AlumnoServiceImpl;
 import com.ramssrez.app.services.IAlumnoService;
 
 public class Main {
     public static void main(String[] args) {
-        IAlumnoService service = new AlumnoServiceImpl();
+        IAlumnoRepository repository = new AlumnoRepositoryImp();
+        IAlumnoService service = new AlumnoServiceImpl(repository);
         Alumno alumno = service.buscarAlumnoPorNombreApllido("Adrian","Gonzalez");
         System.out.println("Alumno encontrado: " + alumno);
     }
